dda8e7f1eb7c939066bd3d5f51bf8b39ec89667d
bruderol <bruderer@computerscience.ch>
2013-06-16 12:29:33 +0200

    Use Firefox for karma tests, does somehow not work with chrome, strange

--- ngUSD-client/karma-e2e.conf.js
+++ ngUSD-client/karma-e2e.conf.js
@@ -41,7 +41,7 @@ autoWatch = false;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome'];
+browsers = ['Firefox'];
 
 // If browser does not capture in given timeout [ms], kill it
 captureTimeout = 5000;
