1e17b1b340ab15f07275f661fb295b890e1a1e73
bruderol <bruderer@computerscience.ch>
2013-06-16 11:52:02 +0200

    Client: created initial project setup with yeoman

---
+++ ngUSD-client/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngUSDClientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
