fabd0405e3f451765c24033d2b83f7d69bdb75f7
bruderol <bruderer@computerscience.ch>
2013-06-16 10:41:59 +0200

    updated VM with bower, grunt and yeoman for angular JS, see http://www.yeoman.io

---
+++ vm-update-scripts/update_2013_06_16_001_install-node-js-and-yeoman.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
+sudo npm install -g yo grunt-cli bower
+sudo npm install -g generator-webapp
+sudo npm install -g generator-angular
+
+
---
+++ vm-update-scripts/update_2013_06_16_001_install-node-js-and-yeoman.sh~
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
+
