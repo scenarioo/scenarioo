00ac5802cc51a91605458c3fb012f5a082436935
bruderol <bruderer@computerscience.ch>
2013-06-17 09:55:07 +0200

    added new vm-update-script to install chrome

---
+++ vm-update-scripts/update_2013_06_17_001_install-chrome-browser.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
+sudo apt-get update
+
+sudo apt-get install google-chrome-stable
