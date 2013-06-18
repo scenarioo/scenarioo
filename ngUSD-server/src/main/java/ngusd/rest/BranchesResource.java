d65c9dc2658d80b098ab0e2eac12851f8b2c31d2
bruderol <bruderer@computerscience.ch>
2013-06-18 08:39:31 +0200

    moved BranchesResources

--- ngUSD-server/src/main/java/ngusd/rest/BranchesResource.java
+++ ngUSD-server/src/main/java/ngusd/rest/BranchesResource.java
@@ -2,14 +2,17 @@ package ngusd.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import ngusd.rest.model.Branch;
 
 @Path("/rest/branches")
-public class Branches {
+public class BranchesResource {
 	
 	@GET
+	@Produces({ "application/xml", "application/json" })
 	public Branch listBranches() {
 		return new Branch("trunk", "main development");
 	}
+	
 }
