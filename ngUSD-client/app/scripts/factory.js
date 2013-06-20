3c8b979a3ce361ab09397118f0cafbf0af70b10c
forkch <news@fork.ch>
2013-06-20 13:45:10 +0200

    added new PagesService to factory

--- ngUSD-client/app/scripts/factory.js
+++ ngUSD-client/app/scripts/factory.js
@@ -3,33 +3,33 @@
 var RestServerPath = "http://localhost:port/ngUSD-server/rest";
 var RestServerPort = ":8050";
 
-NgUsdClientApp.config(function($httpProvider){
-        $httpProvider.defaults.headers.common['Accept'] = 'application/json';
+NgUsdClientApp.config(function ($httpProvider) {
+    $httpProvider.defaults.headers.common['Accept'] = 'application/json';
 });
 
-NgUsdClientApp.factory('BranchService', function($resource) {
+NgUsdClientApp.factory('BranchService', function ($resource) {
 
-    var branchService = $resource(RestServerPath +'/branches', {
+    var branchService = $resource(RestServerPath + '/branches', {
         port: RestServerPort
     }, {});
 
-    branchService.findAllBranches = function(fn) {
+    branchService.findAllBranches = function (fn) {
         return branchService.query(fn);
     };
 
     return branchService;
 });
 
-NgUsdClientApp.factory('UseCaseService',function($resource) {
+NgUsdClientApp.factory('UseCaseService', function ($resource) {
     var useCaseService = $resource(RestServerPath + '/branches/:branchName/builds/:buildName/usecases/:usecaseName',
         {branchName: '@branchName',
-         buildName: '@buildName',
-         usecaseName: '@usecaseName',
-         port: RestServerPort}, {});
+            buildName: '@buildName',
+            usecaseName: '@usecaseName',
+            port: RestServerPort}, {});
 
-    useCaseService.getUseCase = function(branchName, buildName, usecaseName, fn, error) {
-        return useCaseService.get({'branchName': branchName, 'buildName' : buildName, 'usecaseName': usecaseName}, fn, function(response) {
-            if(response.status === 404) {
+    useCaseService.getUseCase = function (branchName, buildName, usecaseName, fn, error) {
+        return useCaseService.get({'branchName': branchName, 'buildName': buildName, 'usecaseName': usecaseName}, fn, function (response) {
+            if (response.status === 404) {
                 error();
             }
         });
@@ -38,14 +38,27 @@ NgUsdClientApp.factory('UseCaseService',function($resource) {
 
 });
 
-NgUsdClientApp.factory('BuildStateService',function($resource) {
+
+NgUsdClientApp.factory('PagesService', function ($resource) {
+    var useCaseService = $resource(RestServerPath + '/branches/:branchName/builds/:buildName/usecases/:usecaseName/scenarios/:scenarioName/pages/:pageName',
+        {branchName: '@branchName',
+            buildName: '@buildName',
+            usecaseName: '@usecaseName',
+            scenarioName: '@scenarioName',
+            pageName: '@pageName',
+            port: RestServerPort}, {});
+
+    return useCaseService;
+
+});
+
+NgUsdClientApp.factory('BuildStateService', function ($resource) {
     var buildStateService = $resource(RestServerPath + '/configuration/buildstates/:state',
         {state: '@state',
-         port: RestServerPort},
+            port: RestServerPort},
         {
             ListBuildStates: { method: "GET", params: {} }
         });
-
 
     return buildStateService;
 
