d9e15287ae03823acbc92107afe9e7709b401dea
Sandra Weber <s.weber@bluemail.ch>
2013-06-21 10:48:22 +0200

    Use branch and build parameter for use case page

--- ngUSD-client/app/scripts/controllers/usecase.js
+++ ngUSD-client/app/scripts/controllers/usecase.js
@@ -1,7 +1,7 @@
 'use strict';
 
-NgUsdClientApp.controller('UseCaseCtrl', ['$scope', '$routeParams', 'UseCaseService', 'BuildStateService', function ($scope, $routeParams, UseCaseService, BuildStateService) {
-    var useCase = UseCaseService.getUseCase('asdf', '123', $routeParams.usecaseName, function (usecase) {
+NgUsdClientApp.controller('UseCaseCtrl', ['$scope', '$location', '$routeParams', 'UseCaseService', 'BuildStateService', 'Config', function ($scope, $location, $routeParams, UseCaseService, BuildStateService, Config) {
+    var useCase = UseCaseService.getUseCase(Config.selectedBranch($location), Config.selectedBuild($location), $routeParams.usecaseName, function (usecase) {
         $scope.usecase = usecase;
         var states = BuildStateService.ListBuildStates(function (states) {
 
