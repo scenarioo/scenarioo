c0cff2f62b6a6c96ed62b34a9df043303d4c0e49
Sandra Weber <s.weber@bluemail.ch>
2013-06-30 22:10:27 +0200

    Added navigation to steps view

--- ngUSD-client/app/scripts/controllers/step.js
+++ ngUSD-client/app/scripts/controllers/step.js
@@ -4,8 +4,8 @@ NgUsdClientApp.controller('StepCtrl', function ($scope, $routeParams, $location,
     var useCaseName = $routeParams.useCaseName;
     var scenarioName = $routeParams.scenarioName;
     $scope.pageName = decodeURIComponent($routeParams.pageName);
-    $scope.pageIndex = $routeParams.pageOccurenceInScenario;
-    $scope.stepIndex = $routeParams.stepIndex;
+    $scope.pageIndex = parseInt($routeParams.pageOccurenceInScenario);
+    $scope.stepIndex = parseInt($routeParams.stepIndex);
 
     var pagesAndScenarios = ScenarioService.getScenario(Config.selectedBranch($location), Config.selectedBuild($location), useCaseName, scenarioName, function(pagesAndScenarios) {
         $scope.scenario = pagesAndScenarios.scenario;
@@ -24,5 +24,48 @@ NgUsdClientApp.controller('StepCtrl', function ($scope, $routeParams, $location,
 
     $scope.getScreenShotUrl = function(imgName) {
         return "http://localhost:8050/ngusd/rest/branches/"+Config.selectedBranch($location)+"/builds/"+Config.selectedBuild($location)+"/usecases/"+useCaseName+"/scenarios/"+scenarioName+"/image/"+imgName;
+    }
+
+    $scope.go = function(pageSteps, pageIndex, stepIndex) {
+        var pageName = pageSteps.page.name;
+        $location.path('/step/' + useCaseName + '/' + scenarioName + '/' + encodeURIComponent(pageName) + '/' + pageIndex + '/' + stepIndex);
+    }
+
+    $scope.goToPreviousPage = function() {
+        var pageIndex = $scope.pageIndex-1;
+        var stepIndex = 0;
+        if (pageIndex<0) {
+            pageIndex = 0;
+        }
+        $scope.go($scope.pagesAndSteps[pageIndex], pageIndex, stepIndex);
+    }
+
+    $scope.goToPreviousStep = function() {
+        var pageIndex = $scope.pageIndex;
+        var stepIndex = $scope.stepIndex-1;
+        if ($scope.stepIndex==0) {
+            pageIndex = $scope.pageIndex-1;
+            stepIndex = $scope.pagesAndSteps[pageIndex].steps.length-1;
+        }
+        $scope.go($scope.pagesAndSteps[pageIndex], pageIndex, stepIndex);
+    }
+
+    $scope.goToNextStep = function() {
+        var pageIndex = $scope.pageIndex;
+        var stepIndex = $scope.stepIndex+1;
+        if (stepIndex >= $scope.pagesAndSteps[$scope.pageIndex].steps.length) {
+            pageIndex = $scope.pageIndex+1;
+            stepIndex = 0;
+        }
+        $scope.go($scope.pagesAndSteps[pageIndex], pageIndex, stepIndex);
+    }
+
+    $scope.goToNextPage = function() {
+        var pageIndex = $scope.pageIndex+1;
+        var stepIndex = 0;
+        if (pageIndex >= $scope.pagesAndSteps.length) {
+            pageIndex = $scope.pagesAndSteps.length-1;
+        }
+        $scope.go($scope.pagesAndSteps[pageIndex], pageIndex, stepIndex);
     }
 });
