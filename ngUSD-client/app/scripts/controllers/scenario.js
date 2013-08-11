71d52f958403bab2656416250c1227081895268e
Sandra Weber <s.weber@bluemail.ch>
2013-08-11 15:50:40 +0200

    Fixed search in scenario view

--- ngUSD-client/app/scripts/controllers/scenario.js
+++ ngUSD-client/app/scripts/controllers/scenario.js
@@ -1,16 +1,17 @@
 'use strict';
 
-NgUsdClientApp.controller('ScenarioCtrl', ['$scope', '$routeParams', '$location', 'ScenarioService','Config', function ($scope, $routeParams,$location, ScenarioService, Config) {
+NgUsdClientApp.controller('ScenarioCtrl', ['$scope', '$filter', '$routeParams', '$location', 'ScenarioService','Config', function ($scope, $filter, $routeParams,$location, ScenarioService, Config) {
     var useCaseName = $routeParams.useCaseName;
     var scenarioName = $routeParams.scenarioName;
     var pagesAndScenarios = ScenarioService.getScenario(Config.selectedBranch($location), Config.selectedBuild($location), useCaseName, scenarioName, function(pagesAndScenarios) {
-        $scope.scenario = pagesAndScenarios.scenario;
-        $scope.pagesAndSteps = pagesAndScenarios.pagesAndSteps;
-        $scope.showingSteps = [];
+        // Add page to the step to allow search for step- as well as page-properties
+        populatePageAndSteps(pagesAndScenarios);
 
         $scope.resetSearchField = function() {
             $scope.searchFieldText = '';
         }
+
+        $scope.showingSteps = [];
 
         $scope.go = function(pageSteps, pageIndex, stepIndex) {
             var pageName = pageSteps.page.name;
@@ -21,6 +22,25 @@ NgUsdClientApp.controller('ScenarioCtrl', ['$scope', '$routeParams', '$location'
             return "http://localhost:8050/ngusd/rest/branches/"+Config.selectedBranch($location)+"/builds/"+Config.selectedBuild($location)+"/usecases/"+useCaseName+"/scenarios/"+scenarioName+"/image/"+imgName;
         }
     });
+
+    function populatePageAndSteps(pagesAndScenarios) {
+        for (var indexPage = 0; indexPage < pagesAndScenarios.pagesAndSteps.length; indexPage++) {
+            var page = pagesAndScenarios.pagesAndSteps[indexPage];
+            page.index = indexPage+1;
+            for (var indexStep = 0; indexStep < page.steps.length; indexStep++) {
+                var step = page.steps[indexStep];
+                step.page = page.page;
+                step.index = indexStep;
+                step.number = (indexStep==0) ? page.index : page.index + "." + indexStep;
+                if (!step.title) {
+                    step.title = 'undefined';
+                }
+            }
+        }
+
+        $scope.scenario = pagesAndScenarios.scenario;
+        $scope.pagesAndSteps = pagesAndScenarios.pagesAndSteps;
+    }
 
     $scope.openScreenshotModal = function() {
         $scope.showingScreenshotModal = true;
@@ -47,5 +67,4 @@ NgUsdClientApp.controller('ScenarioCtrl', ['$scope', '$routeParams', '$location'
         backdropFade: true,
         dialogClass:'modal modal-huge'
     };
-
 }]);
