5453c6167d2cf210e9eefb32f0bfb1f376ffa638
Sandra Weber <s.weber@bluemail.ch>
2013-08-12 21:47:37 +0200

    Added js-beautifier update script, git ignore file for third-party and step resources.

---
+++ ngUSD-server/src/main/java/ngusd/rest/StepResource.java
@@ -0,0 +1,29 @@
+package ngusd.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import ngusd.dao.UserScenarioDocuFilesystem;
+import ngusd.model.docu.entities.Step;
+
+@Path("/rest/branches/{branchName}/builds/{buildName}/usecases/{usecaseName}/scenarios/{scenarioName}/steps/")
+public class StepResource {
+	
+	private final UserScenarioDocuFilesystem filesystem = new UserScenarioDocuFilesystem();
+	
+	/**
+	 * Get a step with all its data (meta data, html, ..)
+	 */
+	@GET
+	@Produces({ "application/xml", "application/json" })
+	@Path("{stepIndex}/")
+	public Step readScenarioWithPagesAndSteps(@PathParam("branchName") final String branchName,
+			@PathParam("buildName") final String buildName, @PathParam("usecaseName") final String usecaseName,
+			@PathParam("scenarioName") final String scenarioName, @PathParam("stepIndex") final int stepIndex) {
+		return filesystem.loadStep(branchName, buildName, usecaseName, scenarioName, stepIndex);
+		
+	}
+	
+}
