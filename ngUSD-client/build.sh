c2205ff3433a3055b498b9d6d5e4f2c046878783
bruderol <bruderer@computerscience.ch>
2013-09-11 21:36:10 +0200

    configure path for grunt in client build script

--- ngUSD-client/build.sh
+++ ngUSD-client/build.sh
@@ -2,8 +2,12 @@
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 
+if [ "$#" -gt 0 ]; then
+    echo "Set path for grunt as configured: $1"
+    export PATH=$PATH:$1
+fi
+
 echo $PATH
-pwd
 
 # TODO remove --force, as soon as all problems are resolved
 grunt build --force
