d475a7b80a5c33bad5a03d177fe6a082a19e03fb
bruderol <bruderer@computerscience.ch>
2013-09-12 00:30:26 +0200

    changed client build script to install also the needed grunt tooling directly integrated in the build. improved gradle build script possible arguments (properties) which are passed to client build script (node JS path and whether to trigger gradle tooling installation)

--- ngUSD-client/build.sh
+++ ngUSD-client/build.sh
@@ -3,12 +3,28 @@ BASEDIR=$(dirname $0)
 cd $BASEDIR
 
 if [ "$#" -gt 0 ]; then
-    echo "Set path for grunt as configured: $1"
+    echo "Set path for nodeJS as configured: $1"
     export PATH=$PATH:$1
 fi
 
-# first install all needed dependencies
+if [ "$#" -gt 1 ]; then
+    if [ "$1" -eq "installGrunt" ]; then
+        echo "Was started with option 'installGrunt", therefore grunt will be installed for building."
+        npm install grunt-cli
+        #npm install less
+        #npm install grunt
+        #npm install grunt-cli
+        #npm install --save-dev grunt-contrib-livereload
+
+        echo "Set path for installed grunt to ./node_modules/grunt-cli/bin/"
+        export PATH=$PATH:./node_modules/grunt-cli/bin/
+    fi
+fi
+
+
+# install all other needed dependencies
 npm install
 
+# let grunt build everything
 # TODO remove --force, as soon as all problems are resolved
 grunt build --force
