58c5173d2da20dfe859be9440faa5272b707ea85
forkch <news@fork.ch>
2013-10-16 14:46:26 +0200

    added updatevm.sh

---
+++ updatevm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# usage: thisscript.sh [directory name]
+directory=$1
+
+vmupdatelog=.vm-update.log
+touch $vmupdatelog
+
+find $directory -name '*.sh' |
+while read filename
+do
+	if grep -Fxq "$filename" $vmupdatelog
+	then
+		echo -e "\e[00;31m>> $filename has already been executed\e[00m"
+	else
+		echo -e "\e[00;32m>> executing $filename \e[00m"
+		$filename
+		echo $filename >> $vmupdatelog
+	fi
+	done
+wait
+
