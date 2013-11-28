5f3ac3e6c78c3f0960bb5dc3d0b8c801abae97a5
adiherzog <adrian.herzog@gmail.com>
2013-11-28 23:29:21 +0100

    Improved test

--- scenarioo-client/test/spec/controllers/main.js
+++ scenarioo-client/test/spec/controllers/main.js
@@ -1,46 +1,39 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller MainCtrl', function () {
 
-    var $controller, $rootScope, $location, $httpBackend, SelectedBranchAndBuild, HostnameAndPort,
-        UseCaseService, BranchesAndBuilds, TestData;
-    var $scope;
-    var MainCtrl;
+    var $location, $httpBackend, HostnameAndPort, TestData, $scope, MainCtrl;
 
     beforeEach(module('scenarioo.controllers'));
 
-    beforeEach(inject(function (_$controller_, _$rootScope_, _$location_, _$httpBackend_, _SelectedBranchAndBuild_,
-                                _UseCaseService_, _BranchesAndBuilds_, _HostnameAndPort_, _TestData_) {
-        $controller = _$controller_;
-        $rootScope = _$rootScope_;
-        $location = _$location_;
-        $httpBackend = _$httpBackend_;
-        SelectedBranchAndBuild = _SelectedBranchAndBuild_;
-        UseCaseService = _UseCaseService_;
-        BranchesAndBuilds = _BranchesAndBuilds_;
-        HostnameAndPort = _HostnameAndPort_;
-        TestData = _TestData_;
+    beforeEach(inject(function ($controller, $rootScope, _$location_, _$httpBackend_, _HostnameAndPort_, _TestData_) {
+            $location = _$location_;
+            $httpBackend = _$httpBackend_;
+            HostnameAndPort = _HostnameAndPort_;
+            TestData = _TestData_;
 
-        $scope = $rootScope.$new();
-        MainCtrl = $controller('MainCtrl', {$scope: $scope, $location: $location,
-            SelectedBranchAndBuild: SelectedBranchAndBuild, UseCaseService: UseCaseService,
-            BranchesAndBuilds: BranchesAndBuilds});
-    }));
+            $scope = $rootScope.$new();
+            MainCtrl = $controller('MainCtrl', {$scope: $scope});
+        }
+    ));
 
-    it('navigates to use case page when use case row is clicked', function () {
-        expect($location.path()).toBe('');
-
-        $scope.goToUseCase('SelectPhoneNumber');
-
-        expect($location.path()).toBe('/usecase/SelectPhoneNumber');
+    it('has no usecases and builds set in the beginning', function () {
+        expect($scope.useCases).toBeUndefined();
+        expect($scope.branchesAndBuilds).toBeUndefined();
     });
 
-    it('loads usecases when branch and build selection changes', function () {
+    it('navigates to use case when link is clicked', function () {
+        expect($location.path()).toBe('');
+
+        $scope.goToUseCase('DisplayWeather');
+
+        expect($location.path()).toBe('/usecase/DisplayWeather');
+    });
+
+    it('loads use cases and builds when branch and build selection changes', function () {
         var USECASES_URL = HostnameAndPort.forTest() + '/scenarioo/rest/branches/release-branch-2014-01-16/builds/example-build/usecases';
         var BRANCHES_URL = HostnameAndPort.forTest() + '/scenarioo/rest/branches';
 
-        expect($scope.useCases).toBeUndefined();
-        expect($scope.branchesAndBuilds).toBeUndefined();
         $httpBackend.whenGET(USECASES_URL).respond(TestData.USECASES);
         $httpBackend.whenGET(BRANCHES_URL).respond(TestData.BRANCHES);
 
