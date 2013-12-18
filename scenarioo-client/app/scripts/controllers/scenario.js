7fd3a85d7be4689fbe318ea6ec03fc3f120e31a1
adiherzog <adrian.herzog@gmail.com>
2013-12-18 17:49:08 +0100

    Improved styling of scenario page.

--- scenarioo-client/app/scripts/controllers/scenario.js
+++ scenarioo-client/app/scripts/controllers/scenario.js
@@ -1,16 +1,16 @@
 /* scenarioo-client
  * Copyright (C) 2014, scenarioo.org Development Team
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -24,6 +24,10 @@ angular.module('scenarioo.controllers').controller('ScenarioCtrl', function ($sc
     var selectedBranchAndBuild;
 
     $scope.showingSteps = [];
+
+    $scope.toggleShowAllStepsForPage = function(pageIndex) {
+        $scope.showingSteps[pageIndex] = !$scope.showingSteps[pageIndex];
+    };
 
     $scope.modalScreenshotOptions = {
         backdropFade: true,
@@ -74,10 +78,10 @@ angular.module('scenarioo.controllers').controller('ScenarioCtrl', function ($sc
             '/usecases/' + useCaseName + '/scenarios/' + scenarioName + '/image/' + imgName;
     };
 
-    $scope.goToStep = function (pageSteps, pageIndex, stepIndex) {
+    $scope.getLinkToStep = function (pageSteps, pageIndex, stepIndex) {
         var pageName = pageSteps.page.name;
-        $location.path('/step/' + useCaseName + '/' + scenarioName + '/' + encodeURIComponent(pageName) +
-            '/' + pageIndex + '/' + stepIndex);
+        return '#/step/' + useCaseName + '/' + scenarioName + '/' + encodeURIComponent(pageName) +
+            '/' + pageIndex + '/' + stepIndex;
     };
 
     $scope.resetSearchField = function () {
