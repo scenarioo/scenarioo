83f8a56d4680ca7f16698485c98ac1eede3d373a
bruderol <bruderer@computerscience.ch>
2013-12-20 02:36:53 +0100

    commited generated XSD and changed name for XSD with namespaces.

---
+++ scenarioo-api/src/main/java/org/scenarioo/model/docu/entities/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Provides the basic elements out of which the documentation files consist. 
+ */
+@javax.xml.bind.annotation.XmlSchema(
+		xmlns = {
+				@javax.xml.bind.annotation.XmlNs(prefix = "",
+						namespaceURI = "http://www.scenarioo.org/scenarioo"),
+				@javax.xml.bind.annotation.XmlNs(prefix = "xs",
+						namespaceURI = "http://www.w3.org/2001/XMLSchema")
+		})
+package org.scenarioo.model.docu.entities;
