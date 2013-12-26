ec04a5b7c5ed409622a965ef190013f281dc8898
forkch <bm@fork.ch>
2013-12-26 09:37:05 +0100

    using phantom js for karm e2e tests

--- scenarioo-client/karma-e2e.conf.js
+++ scenarioo-client/karma-e2e.conf.js
@@ -58,7 +58,7 @@ autoWatch = false;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Firefox'];
+browsers = ['PhantomJS'];
 
 // If browser does not capture in given timeout [ms], kill it
 captureTimeout = 5000;
