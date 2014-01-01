d62ff157806fd406bae6924c3ca3f1525b84afb4
Benjamin Müller <bm@fork.ch>
2014-01-01 15:41:16 +0100

    added classpath exception to all licence headers

--- scenarioo-api/src/main/java/org/scenarioo/api/files/ScenarioDocuFiles.java
+++ scenarioo-api/src/main/java/org/scenarioo/api/files/ScenarioDocuFiles.java
@@ -5,6 +5,11 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
+ *
+ * As a special exception, the copyright holders of this library give you 
+ * permission to link this library with independent modules, according 
+ * to the GNU General Public License with "Classpath" exception as provided
+ * in the LICENSE file that accompanied this code.
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
--- scenarioo-api/src/main/java/org/scenarioo/model/docu/derived/BuildLink.java
+++ scenarioo-api/src/main/java/org/scenarioo/model/docu/derived/BuildLink.java
@@ -5,6 +5,11 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
+ *
+ * As a special exception, the copyright holders of this library give you 
+ * permission to link this library with independent modules, according 
+ * to the GNU General Public License with "Classpath" exception as provided
+ * in the LICENSE file that accompanied this code.
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,6 +19,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 package org.scenarioo.model.docu.derived;
 
