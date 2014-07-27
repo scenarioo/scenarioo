7173385144fde30c390fa9ac1cdbc535b98bc16d
adiherzog <adrian.herzog@gmail.com>
2014-07-28 00:53:35 +0200

    #270 Show API version and internal data format version in info popup.

--- scenarioo-server/src/main/java/org/scenarioo/rest/application/ApplicationVersion.java
+++ scenarioo-server/src/main/java/org/scenarioo/rest/application/ApplicationVersion.java
@@ -7,25 +7,38 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ApplicationVersion {
-
+	
 	public ApplicationVersion() {
 		// For REST
 	}
-
-	public ApplicationVersion(final String version, final String buildDate) {
+	
+	public ApplicationVersion(final String version, final String buildDate, final String apiVersion,
+			final String aggregatedDataFormatVersion) {
 		this.version = version;
 		this.buildDate = buildDate;
+		this.apiVersion = apiVersion;
+		this.aggregatedDataFormatVersion = aggregatedDataFormatVersion;
 	}
-
+	
 	private String version;
 	private String buildDate;
-
+	private String apiVersion;
+	private String aggregatedDataFormatVersion;
+	
 	public String getVersion() {
 		return version;
 	}
-
+	
 	public String getBuildDate() {
 		return buildDate;
 	}
-
+	
+	public String getApiVersion() {
+		return apiVersion;
+	}
+	
+	public String getAggregatedDataFormatVersion() {
+		return aggregatedDataFormatVersion;
+	}
+	
 }
