ad94c042a1bc9f32e499220c50bc8cebc749f46f
adiherzog <adrian.herzog@gmail.com>
2014-08-11 00:05:30 +0200

    #240 mark fallback images

--- scenarioo-server/src/main/java/org/scenarioo/rest/ScreenshotResource.java
+++ scenarioo-server/src/main/java/org/scenarioo/rest/ScreenshotResource.java
@@ -18,16 +18,17 @@
 package org.scenarioo.rest;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.DecimalFormat;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import org.jboss.resteasy.specimpl.ResponseBuilderImpl;
 import org.scenarioo.api.ScenarioDocuReader;
 import org.scenarioo.business.builds.ScenarioDocuBuildsManager;
 import org.scenarioo.dao.aggregates.AggregatedDataReader;
@@ -37,6 +38,7 @@ import org.scenarioo.model.docu.aggregates.objects.LongObjectNamesResolver;
 import org.scenarioo.rest.request.BuildIdentifier;
 import org.scenarioo.rest.request.ScenarioIdentifier;
 import org.scenarioo.rest.request.StepIdentifier;
+import org.scenarioo.rest.util.FallbackImageMarker;
 import org.scenarioo.rest.util.ScenarioLoader;
 import org.scenarioo.rest.util.StepImageInfo;
 import org.scenarioo.rest.util.StepImageInfoLoader;
@@ -54,6 +56,7 @@ public class ScreenshotResource {
 	private final ScenarioLoader scenarioLoader = new ScenarioLoader(aggregatedDataReader);
 	private final StepIndexResolver stepIndexResolver = new StepIndexResolver();
 	private final StepImageInfoLoader stepImageInfoLoader = new StepImageInfoLoader(scenarioLoader, stepIndexResolver);
+	private final FallbackImageMarker fallbackImageMarker = new FallbackImageMarker();
 	
 	private final String PNG_FILE_EXTENSION = ".png";
 	
@@ -72,7 +75,7 @@ public class ScreenshotResource {
 				buildName);
 		ScenarioIdentifier scenarioIdentifier = new ScenarioIdentifier(buildIdentifier, usecaseName, scenarioName);
 		
-		return createFoundImageResponse(scenarioIdentifier, imageFileName);
+		return createFoundImageResponse(scenarioIdentifier, imageFileName, false);
 	}
 	
 	/**
@@ -87,7 +90,8 @@ public class ScreenshotResource {
 			@PathParam("buildName") final String buildName, @PathParam("usecaseName") final String usecaseName,
 			@PathParam("scenarioName") final String scenarioName, @PathParam("pageName") final String pageName,
 			@PathParam("pageOccurrence") final int pageOccurrence,
-			@PathParam("stepInPageOccurrence") final int stepInPageOccurrence) {
+			@PathParam("stepInPageOccurrence") final int stepInPageOccurrence,
+			@QueryParam("fallback") final boolean showFallbackStamp) {
 		
 		BuildIdentifier buildIdentifier = ScenarioDocuBuildsManager.INSTANCE.resolveBranchAndBuildAliases(branchName,
 				buildName);
@@ -97,13 +101,14 @@ public class ScreenshotResource {
 		StepImageInfo stepImageInfo = stepImageInfoLoader.loadStepImageInfo(stepIdentifier);
 		
 		// TODO [fallback / aliases] Use alias for branch / build in case one was used
-		return createResponse(stepImageInfo);
+		return createResponse(stepImageInfo, showFallbackStamp);
 	}
 	
-	private Response createResponse(final StepImageInfo stepImage) {
+	private Response createResponse(final StepImageInfo stepImage, final boolean showFallbackStamp) {
 		if (stepImage.isRequestedStepFound()) {
 			String imageFileName = getFileName(stepImage.getStepIndex());
-			return createFoundImageResponse(stepImage.getStepIdentifier().getScenarioIdentifier(), imageFileName);
+			return createFoundImageResponse(stepImage.getStepIdentifier().getScenarioIdentifier(), imageFileName,
+					showFallbackStamp);
 		} else if (stepImage.isRedirect()) {
 			return createRedirectResponse(stepImage.getStepIdentifier());
 		} else {
@@ -116,7 +121,7 @@ public class ScreenshotResource {
 	}
 	
 	private Response createImageNotFoundResponse() {
-		return new ResponseBuilderImpl().status(Status.NOT_FOUND).build();
+		return Response.status(Status.NOT_FOUND).build();
 	}
 	
 	private String getFileName(final int stepIndex) {
@@ -124,7 +129,8 @@ public class ScreenshotResource {
 		return decimalFormat.format(stepIndex) + PNG_FILE_EXTENSION;
 	}
 	
-	private Response createFoundImageResponse(final ScenarioIdentifier scenarioIdentifier, final String imageFileName) {
+	private Response createFoundImageResponse(final ScenarioIdentifier scenarioIdentifier, final String imageFileName,
+			final boolean showFallbackStamp) {
 		final BuildIdentifier buildIdentifier = scenarioIdentifier.getBuildIdentifier();
 		final String usecaseName = scenarioIdentifier.getUsecaseName();
 		final String scenarioName = scenarioIdentifier.getScenarioName();
@@ -132,15 +138,29 @@ public class ScreenshotResource {
 		File screenshot = scenarioDocuReader.getScreenshotFile(buildIdentifier.getBranchName(),
 				buildIdentifier.getBuildName(), usecaseName, scenarioName, imageFileName);
 		
-		return createResponse(imageFileName, screenshot);
+		return createResponse(imageFileName, screenshot, showFallbackStamp);
 	}
 	
-	private Response createResponse(final String imgName, final File screenshot) {
+	private Response createResponse(final String imgName, final File screenshot, final boolean showFallbackStamp) {
 		if (screenshot == null || !screenshot.exists()) {
-			return Response.status(Status.NOT_FOUND).build();
+			return createImageNotFoundResponse();
 		}
 		
-		return createOkResponse(imgName, screenshot);
+		if (showFallbackStamp) {
+			return createOkResponseWithFallbackStamp(imgName, screenshot);
+		} else {
+			return createOkResponse(imgName, screenshot);
+		}
+	}
+	
+	private Response createOkResponseWithFallbackStamp(final String imgName, final File screenshot) {
+		try {
+			byte[] stampedScreenshot = fallbackImageMarker.getMarkedImage(screenshot);
+			return Response.ok(stampedScreenshot).build();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return createImageNotFoundResponse();
+		}
 	}
 	
 	private Response createOkResponse(final String imgName, final File screenshot) {
