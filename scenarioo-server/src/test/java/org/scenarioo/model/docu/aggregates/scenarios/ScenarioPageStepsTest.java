3c28a22c6e0d9069304f8d1ec259dbb275d27cff
adiherzog <adrian.herzog@gmail.com>
2014-09-08 20:23:42 +0200

    Filled empty code block.

--- scenarioo-server/src/test/java/org/scenarioo/model/docu/aggregates/scenarios/ScenarioPageStepsTest.java
+++ scenarioo-server/src/test/java/org/scenarioo/model/docu/aggregates/scenarios/ScenarioPageStepsTest.java
@@ -10,58 +10,58 @@ import org.scenarioo.model.docu.entities.Page;
 import org.scenarioo.model.docu.entities.StepDescription;
 
 public class ScenarioPageStepsTest {
-
+	
 	private ScenarioPageSteps scenarioPageSteps;
 	private int total;
 	private StepStatistics stepStatistics;
-
+	
 	@Test
 	public void getTotalNumberOfStepsInScenario_nullList() {
 		givenScenarioPagesAndStepsWithNullList();
-
+		
 		whenGettingTotal();
-
+		
 		expectTotalNumberOfStepsEquals(0);
 	}
-
+	
 	@Test
 	public void getTotalNumberOfStepsInScenario_moreThanZeroSteps() {
 		givenScenarioPagesAndSteps();
-
+		
 		whenGettingTotal();
-
+		
 		expectTotalNumberOfStepsEquals(7);
 	}
-
+	
 	@Test
 	public void getTotalNumberOfStepsInPageOccurrence() {
 		givenScenarioPagesAndSteps();
-
+		
 		whenGettingTotalStepsForPage1SecondOccurrence();
-
+		
 		expectTotalNumberOfStepsEquals(4);
 	}
-
+	
 	@Test
 	public void getStepStatistics() {
 		givenScenarioPagesAndSteps();
-
+		
 		whenGettingPageStatisticsForPage1SecondOccurrence();
-
+		
 		expectTotalNumberOfStepsInStatisticsEquals(7);
 		expectTotalNumberOfStepsInPageOccurrenceInStatisticsEquals(4);
 		expectTotalNumberOfPagesInScenarioInStatisticsEquals(3);
 	}
-
+	
 	private void givenScenarioPagesAndStepsWithNullList() {
 		scenarioPageSteps = new ScenarioPageSteps();
 	}
-
+	
 	private void givenScenarioPagesAndSteps() {
 		scenarioPageSteps = new ScenarioPageSteps();
 		scenarioPageSteps.setPagesAndSteps(createPagesAndStepsList());
 	}
-
+	
 	private List<PageSteps> createPagesAndStepsList() {
 		List<PageSteps> pageSteps = new LinkedList<PageSteps>();
 		pageSteps.add(createPageStepsWithNumberOfSteps(1, "page1"));
@@ -69,62 +69,58 @@ public class ScenarioPageStepsTest {
 		pageSteps.add(createPageStepsWithNumberOfSteps(4, "page1"));
 		return pageSteps;
 	}
-
-	private PageSteps createPageStepsWithNumberOfSteps(final int numberOfSteps,
-			final String pageName) {
+	
+	private PageSteps createPageStepsWithNumberOfSteps(final int numberOfSteps, final String pageName) {
 		PageSteps pageSteps = new PageSteps();
 		pageSteps.setPage(createPage(pageName));
 		pageSteps.setSteps(createStepsListWithNumberOfSteps(numberOfSteps));
 		return pageSteps;
 	}
-
+	
 	private Page createPage(final String pageName) {
 		Page page = new Page();
 		page.setName(pageName);
 		return page;
 	}
-
-	private List<StepDescription> createStepsListWithNumberOfSteps(
-			final int numberOfSteps) {
+	
+	private List<StepDescription> createStepsListWithNumberOfSteps(final int numberOfSteps) {
 		List<StepDescription> stepDescription = new LinkedList<StepDescription>();
 		for (int i = 0; i < numberOfSteps; i++) {
 			stepDescription.add(createStep());
 		}
 		return stepDescription;
 	}
-
+	
 	private StepDescription createStep() {
 		return new StepDescription();
 	}
-
+	
 	private void whenGettingTotal() {
 		total = scenarioPageSteps.getTotalNumberOfStepsInScenario();
 	}
-
+	
 	private void whenGettingTotalStepsForPage1SecondOccurrence() {
-		total = scenarioPageSteps.getTotalNumberOfStepsInPageOccurrence(
-				"page1", 1);
+		total = scenarioPageSteps.getTotalNumberOfStepsInPageOccurrence("page1", 1);
 	}
-
+	
 	private void whenGettingPageStatisticsForPage1SecondOccurrence() {
 		stepStatistics = scenarioPageSteps.getStepStatistics("page1", 1);
 	}
-
+	
 	private void expectTotalNumberOfStepsEquals(final int expectedTotal) {
 		Assert.assertEquals(expectedTotal, total);
 	}
-
+	
 	private void expectTotalNumberOfStepsInStatisticsEquals(final int i) {
 		Assert.assertEquals(i, stepStatistics.getTotalNumberOfStepsInScenario());
 	}
-
-	private void expectTotalNumberOfStepsInPageOccurrenceInStatisticsEquals(
-			final int i) {
+	
+	private void expectTotalNumberOfStepsInPageOccurrenceInStatisticsEquals(final int i) {
+		Assert.assertEquals(i, stepStatistics.getTotalNumberOfStepsInPageOccurrence());
 	}
-
-	private void expectTotalNumberOfPagesInScenarioInStatisticsEquals(
-			final int i) {
+	
+	private void expectTotalNumberOfPagesInScenarioInStatisticsEquals(final int i) {
 		Assert.assertEquals(i, stepStatistics.getTotalNumberOfPagesInScenario());
 	}
-
+	
 }
