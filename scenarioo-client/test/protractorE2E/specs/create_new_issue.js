a474255deb63a1e21bffbe811c8c5af134f53e7c
myrion <myrion_the_first@hotmail.com>
2015-05-06 09:46:39 +0200

    Added two new e2e tests Added IDs to the issue tab to enable the e2e tests

--- scenarioo-client/test/protractorE2E/specs/create_new_issue.js
+++ scenarioo-client/test/protractorE2E/specs/create_new_issue.js
@@ -13,7 +13,59 @@ scenarioo.describeUseCase('Create a new Issue', function () {
 
     homePage.selectIssuesTab();
 
+    var numberOfIssues = 0;
+    var issuesList = element(by.css('table')).findElements(by.css('tbody tr')).then(function (elements) {
+      this.numberOfIssues = elements.length;
+    });
 
+
+    var newIssueName = element(by.model("issueName"));
+    var newIssueDesc = element(by.id("issueDescription"));
+
+    newIssueName.sendKeys("This is my Test issue name" + Date.now());
+    newIssueDesc.sendKeys("And this is the description");
+
+    var saveButton = element(by.id('addBtn'));
+
+    saveButton.click();
+
+    issuesList = element(by.css('table')).findElements(by.css('tbody tr')).then(function (elements) {
+      expect(elements.length).toBe(this.numberOfIssues + 1);
+    });
+
+  });
+
+  scenarioo.describeScenario('Create a new issue with a too long name', function () {
+    var homePage = new pages.homePage();
+
+    browser.get('#/');
+    homePage.closeScenariooInfoDialogIfOpen();
+
+    homePage.selectIssuesTab();
+
+    var numberOfIssues = 0;
+    var issuesList = element(by.css('table')).findElements(by.css('tbody tr')).then(function (elements){
+      this.numberOfIssues = elements.length;
+    });
+
+
+    var newIssueName = element(by.model("issueName"));
+    var newIssueDesc = element(by.id("issueDescription"));
+
+    newIssueName.sendKeys("This is my Test issue name which is going to be way too long" +
+                          "because I make it way too long in order to test that it does not" +
+                          "get created as we do not want names to be so long" +
+                          "although eventually this test should be wrong since we can shorten names" +
+                          "once we start using the longobjectnamesresolver");
+    newIssueDesc.sendKeys("And this is the description");
+
+    var saveButton = element(by.id('addBtn'));
+
+    saveButton.click();
+
+    issuesList = element(by.css('table')).findElements(by.css('tbody tr')).then(function (elements){
+      expect(elements.length).toBe(this.numberOfIssues);
+    });
 
   });
 
