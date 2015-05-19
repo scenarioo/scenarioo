836e0d083e2e82a3bbb806309ea5bc09fabf8265
myrion <myrion_the_first@hotmail.com>
2015-05-19 09:37:33 +0200

    #72 Enabled storing ScenarioSketches Changed methods which store Objects on the server to return explicit responses Storing an issue returns the entire issue with newly created ID

--- scenarioo-server/src/main/java/org/scenarioo/rest/proposal/ScenarioSketchesResource.java
+++ scenarioo-server/src/main/java/org/scenarioo/rest/proposal/ScenarioSketchesResource.java
@@ -1,13 +1,18 @@
 package org.scenarioo.rest.proposal;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 import org.scenarioo.business.builds.ScenarioDocuBuildsManager;
 import org.scenarioo.dao.design.aggregates.IssueAggregationDAO;
+import org.scenarioo.dao.design.entities.DesignFiles;
 import org.scenarioo.model.design.aggregates.ScenarioSketchSteps;
+import org.scenarioo.model.design.entities.ScenarioSketch;
 import org.scenarioo.repository.ConfigurationRepository;
 import org.scenarioo.repository.RepositoryLocator;
 import org.scenarioo.rest.base.BuildIdentifier;
@@ -26,23 +31,34 @@ public class ScenarioSketchesResource {
 			configurationRepository.getDesignDataDirectory());
 
 	private final DesignReader reader = new DesignReader(configurationRepository.getDesignDataDirectory());
+	private final DesignFiles files = new DesignFiles(configurationRepository.getDesignDataDirectory());
 
 	private final ScenarioDetailsMapper scenarioDetailsMapper = new ScenarioDetailsMapper();
 
 	@GET
 	@Produces({ "application/xml", "application/json" })
-	public ScenarioSketchSteps loadProposal(@PathParam("branchName") final String branchName,
-			@PathParam("issueName") final String issueName,
-			@PathParam("proposalName") final String proposalName) {
+	public ScenarioSketchSteps loadScenarioSketch(@PathParam("branchName") final String branchName,
+			@PathParam("issueId") final String issueId,
+			@PathParam("proposalName") final String scenarioSketchName) {
 
 		BuildIdentifier buildIdentifier = ScenarioDocuBuildsManager.INSTANCE.resolveBranchAndBuildAliases(branchName,
 				"");
-		ScenarioSketchIdentifier proposalIdentifier = new ScenarioSketchIdentifier(buildIdentifier, issueName, proposalName);
+		ScenarioSketchIdentifier proposalIdentifier = new ScenarioSketchIdentifier(buildIdentifier, issueId,
+				scenarioSketchName);
 
 		ScenarioSketchSteps scenarioSketchSteps = dao.loadScenarioSketchSteps(proposalIdentifier);
 
 		// TODO: Investigate whether ProposalDetails and ProposalMapper are necessary
 		return scenarioSketchSteps;
+	}
+
+	@POST
+	@Consumes({ "application/xml", "application/json" })
+	public Response storeScenarioSketch(@PathParam("branchName") final String branchName,
+			@PathParam("issueId") final String issueId,
+			final ScenarioSketch scenarioSketch) {
+		files.writeScenarioSketchToFile(branchName, issueId, scenarioSketch);
+		return Response.ok().build();
 	}
 
 }
