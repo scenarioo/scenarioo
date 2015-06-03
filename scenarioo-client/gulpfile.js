4cc0128654bcba7fe4b4409ba751b8c3077d3cff
adiherzog <adrian.herzog@gmail.com>
2015-06-03 20:29:08 +0200

    #407 Documented our new gulpfile.js a bit more.

--- scenarioo-client/gulpfile.js
+++ scenarioo-client/gulpfile.js
@@ -1,4 +1,4 @@
-/*eslint-env node */
+/* eslint-env node */
 
 var gulp = require('gulp'),
     del = require('del'),
@@ -26,8 +26,10 @@ var files = {
     less: ['./app/styles/*.less']
 };
 
+
 /**
- *
+ * Serve the 'app' folder as a web application. The page is automatically reloaded whenever a file changes.
+ * Use 'gulp serve' to view the app during development.
  */
 gulp.task('serve', ['environmentConstants', 'watch'], function () {
     connect.server({
@@ -37,9 +39,21 @@ gulp.task('serve', ['environmentConstants', 'watch'], function () {
     });
 });
 
+/**
+ * Build the application and serve the 'dist' folder in a web server. Files are not watched, so you have
+ * to build again and reload the page yourself after a change.
+ * Use this to test your built Scenarioo client.
+ */
+gulp.task('serve-dist', ['build'], function () {
+    connect.server({
+        root: 'dist',
+        livereload: true,
+        port: 9000
+    });
+});
 
 /**
- * lint our sources with ESLint (http://eslint.org/)
+ * Lint our sources with ESLint (http://eslint.org/).
  */
 gulp.task('lint', function () {
     return gulp.src(_.flatten([files.sources, files.tests, 'gulpfile.js']))
@@ -48,29 +62,26 @@ gulp.task('lint', function () {
         .pipe(eslint.failAfterError());
 });
 
+/**
+ * Watches our source files and reloads them in the developers browser, when changes are made.
+ * Also compiles the less files to css whenever changes are made.
+ */
+gulp.task('watch', function () {
+    gulp.watch(files.css, files.templates, files.views, files.sources, ['reload-files']);
+    gulp.watch(files.less, ['less', 'reload-files']);
+});
 
 /**
- * will serve the dist folder on port 9000.
- * Use this to test your built jetpack client.
+ * Reloads the page in the 'gulp serve' browser. Called when source files are changed.
  */
-gulp.task('serveDist', ['build'], function () {
-    connect.server({
-        root: 'dist',
-        livereload: true,
-        port: 9000
-    });
-});
-
-gulp.task('watch', function () {
-    gulp.watch(files.css, files.templates, files.views, files.sources, ['file-reload']);
-    gulp.watch(files.less, ['less', 'file-reload']);
-});
-
-gulp.task('file-reload', function () {
+gulp.task('reload-files', function () {
     gulp.src(files.css, files.templates, files.views, files.sources, files.less)
         .pipe(connect.reload());
 });
 
+/**
+ * Compiles our less files into css.
+ */
 gulp.task('less', function () {
     return gulp.src(files.less)
         .pipe(less().on('error', function (e) {
@@ -79,6 +90,9 @@ gulp.task('less', function () {
         .pipe(gulp.dest('./app/styles/'));
 });
 
+/**
+ * Run unit tests once
+ */
 gulp.task('test', ['environmentConstants'], function (done) {
     karma.start({
         configFile: path.join(__dirname, 'karma.conf.js'),
@@ -86,13 +100,20 @@ gulp.task('test', ['environmentConstants'], function (done) {
     }, done);
 });
 
+/**
+ * Run unit tests after each file change
+ */
 gulp.task('test-watch', function (done) {
     karma.start({
         configFile: path.join(__dirname, 'karma.conf.js'),
-        singleRun: false
+        singleRun: false,
+        autoWatch: true
     }, done);
 });
 
+/**
+ * Run protractor ui tests (without generating Scenarioo documentation)
+ */
 gulp.task('test-e2e', function () {
     gulp.src(['./test/protractorE2E/specs/**/*.js'])
         .pipe(protractor({
@@ -104,40 +125,48 @@ gulp.task('test-e2e', function () {
 });
 
 /**
- * read constants from environments.json and write angular config file "environment_config.js"
+ * Run protractor ui tests and generate Scenarioo documentation from them.
+ * TODO Fix this, see https://github.com/scenarioo/scenarioo/issues/410
+ */
+gulp.task('test-e2e-scenarioo', function () {
+    gulp.src(['./test/protractorE2E/specs/**/*.js'])
+        .pipe(protractor({
+            configFile: './protractor-e2e-scenarioo.conf.js'
+        }))
+        .on('error', function (e) {
+            throw e;
+        });
+});
+
+/**
+ * Read constants from environments.json and write angular config file "environment_config.js"
  * specify environment like so:
  *
  * gulp serve --production
  * gulp build --production
  *
- * default is "development" and does not need to be specified
+ * Default is "development" and does not need to be specified.
  */
 gulp.task('environmentConstants', function (done) {
-
     var environments = require('./environments.json');
     var selectedEnvironment = gulpUtil.env.production ? 'production' : 'development';
 
-    var angularConfigFileContent = '';
-
-    angularConfigFileContent += '// this file is written by a gulp task. configuration is done in environments.json\n';
+    var angularConfigFileContent = '// This file is written by a gulp task. Configuration is done in environments.json\n\n';
     angularConfigFileContent += 'angular.module(\'scenarioo.config\', [])\n';
-
-
     _.forEach(environments[selectedEnvironment], function (value, key) {
         angularConfigFileContent += '.constant(\'' + key + '\', \'' + value + '\')\n';
     });
     angularConfigFileContent += '.constant(\'ENV\', \'' + selectedEnvironment + '\');\n';
 
-
     fs.writeFile('./app/scripts/environment_config.js', angularConfigFileContent, done);
-
 });
 
-
-gulp.task('cleanDist', function (done) {
+/**
+ * Delete the 'dist' folder.
+ */
+gulp.task('clean-dist', function (done) {
     del('./dist', done);
 });
-
 
 /**
  *  Wraps given files (pipe) with IIFE and adds 'use strict';
@@ -146,7 +175,10 @@ function wrapWithIIFE() {
     return wrap('(function(){\n\'use strict\';\n <%= contents %> }());\n');
 }
 
-gulp.task('usemin', ['cleanDist'], function () {
+/**
+ * Concatenate and uglify sources.
+ */
+gulp.task('usemin', ['clean-dist'], function () {
     return gulp.src('./app/index.html')
         .pipe(usemin({
             sources: [wrapWithIIFE(), 'concat', ngAnnotate(), uglify()],
@@ -161,7 +193,7 @@ gulp.task('usemin', ['cleanDist'], function () {
 /**
  * Copies all required files from the 'app' folder to the 'dist' folder.
  */
-gulp.task('copyAssets', ['environmentConstants', 'cleanDist', 'usemin', 'less'], function () {
+gulp.task('copy-to-dist', ['environmentConstants', 'clean-dist', 'usemin', 'less'], function () {
     /* copy own images, styles, and templates */
     gulp.src(files.images).pipe(gulp.dest('./dist/images'));
     gulp.src(files.css).pipe(gulp.dest('./dist/styles'));
@@ -174,6 +206,12 @@ gulp.task('copyAssets', ['environmentConstants', 'cleanDist', 'usemin', 'less'],
     gulp.src(['./app/components/bootstrap/dist/fonts/*']).pipe(gulp.dest('./dist/fonts'));
 });
 
-gulp.task('build', ['lint', 'test', 'cleanDist', 'copyAssets']);
+/**
+ * Build the client.
+ */
+gulp.task('build', ['lint', 'test', 'clean-dist', 'copy-to-dist']);
 
+/**
+ * If you call gulp without a target, we assume you want to build the client.
+ */
 gulp.task('default', ['build']);
