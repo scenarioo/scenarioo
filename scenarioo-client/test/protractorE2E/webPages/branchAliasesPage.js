19282ad80d5cf8bdd0b005c01e5bc5d8ebf7ec19
bruderol <bruderer@computerscience.ch>
2015-06-17 11:26:32 +0200

    #404 corrected broken e2e test error

--- scenarioo-client/test/protractorE2E/webPages/branchAliasesPage.js
+++ scenarioo-client/test/protractorE2E/webPages/branchAliasesPage.js
@@ -32,10 +32,11 @@ BranchAliasesPage.prototype.openBranchSelectionMenu = function () {
 
 BranchAliasesPage.prototype.assertAliasesAreShownFirstInTheNavigationMenu = function () {
     var branchOptions = element.all(by.css('#branchSelectionDropdown .branchOption'));
-    expect(branchOptions.count()).toBeGreaterThan(2); // test data branches: 2 aliases + at least one branch
+    var count = branchOptions.count();
+    expect(count).toBeGreaterThan(2); // test data branches: 2 aliases + at least one branch
     expect(branchOptions.get(0).getText()).toBe('Test Alias 1 (wikipedia-docu-example)');
     expect(branchOptions.get(1).getText()).toBe('Test Alias 2 (wikipedia-docu-example)');
-    expect(branchOptions.get(branchOptions.count() - 1).getText()).toBe('wikipedia-docu-example'); // currently assuming that it has to be the last one when starting with "w"
+    expect(branchOptions.get(count - 1).getText()).toBe('wikipedia-docu-example'); // currently assuming that it has to be the last one when starting with "w"
 };
 
 BranchAliasesPage.prototype.enterAlias = function (name, referencedBranchName, description) {
