284119435710415c35263584a7566c86fe5e702b
bruderol <bruderer@computerscience.ch>
2015-06-17 13:31:38 +0200

    #404 improved failing web test for better reporting

--- scenarioo-client/test/protractorE2E/specs/manage_branch_aliases.js
+++ scenarioo-client/test/protractorE2E/specs/manage_branch_aliases.js
@@ -19,10 +19,11 @@ scenarioo.describeUseCase('Manage branch aliases', function () {
         branchAliasesPage.enterAlias('Test Alias 1', 'wikipedia-docu-example', 'my description 1');
         branchAliasesPage.enterAlias('Test Alias 2', 'wikipedia-docu-example', 'my description 2');
         branchAliasesPage.save();
-        scenarioo.docuWriter.saveStep('add build aliases');
+        scenarioo.docuWriter.saveStep('saved build aliases');
         branchAliasesPage.reset();
         branchAliasesPage.assertNumberOfAliases(2);
         branchAliasesPage.openBranchSelectionMenu();
+        scenarioo.docuWriter.saveStep('open branch menu with aliases');
         branchAliasesPage.assertAliasesAreShownFirstInTheNavigationMenu();
 
         branchAliasesPage.deleteAlias(0);
@@ -30,14 +31,17 @@ scenarioo.describeUseCase('Manage branch aliases', function () {
         branchAliasesPage.assertNumberOfAliases(1);
         branchAliasesPage.reset();
         branchAliasesPage.assertNumberOfAliases(1);
+        scenarioo.docuWriter.saveStep('removed first alias');
 
         branchAliasesPage.updateAlias(0, 'updated alias', 'wikipedia-docu-example', 'updated description');
         branchAliasesPage.save();
-        scenarioo.docuWriter.saveStep('update aliases');
+        scenarioo.docuWriter.saveStep('updated first alias');
 
         branchAliasesPage.deleteAlias(0);
         branchAliasesPage.deleteAlias(0);
         branchAliasesPage.save();
+        scenarioo.docuWriter.saveStep('all aliases removed');
+
     });
 
     scenarioo.describeScenario('Saving is not possible if referenced branch is not selected', function() {
