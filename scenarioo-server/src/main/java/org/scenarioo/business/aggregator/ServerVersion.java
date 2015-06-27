6f90704fb188dc787bae688b4501699a5b33878a
bruderol <bruderer@computerscience.ch>
2015-06-27 15:13:21 +0200

    #149 Screen Annotations: increased internal server format version because of changes to object repository for Screen Annotations and refactored derived format version into own ServerVersion class (easier to find in case it needs to be increased).

--- scenarioo-server/src/main/java/org/scenarioo/business/aggregator/ServerVersion.java
+++ scenarioo-server/src/main/java/org/scenarioo/business/aggregator/ServerVersion.java
@@ -2,19 +2,18 @@ package org.scenarioo.business.aggregator;
 
 /**
  * Container for the current internal format version that this server supports.
- * 
- * First part of the version corresponds to library version that is supported, second part of the version depends on
- * internal aggregation format.
- * 
- * On import this version is stored for each build, if the format of a build does not correspond to this format, the
- * build is automatically reimported again (all aggregation data recalculated in new format).
  */
 public class ServerVersion {
 
 	/**
 	 * Version of the internal file format of derived files in filesystem.
 	 * 
-	 * he data aggregator checks whether the file format is the same, otherwise the data has to be recalculated.
+	 * On import this version is stored for each build, if the format of a build does not correspond to this format, the
+	 * build is automatically reimported again (all aggregation data recalculated in new format).
+	 * 
+	 * First part of the version corresponds to library version that is supported, second part of the version depends on
+	 * internal aggregation format. the second part should be increased whenever something important is changed in the
+	 * internal format or the way that the aggregator is caluclating internal data on builds.
 	 */
 	public static final String DERIVED_FILE_FORMAT_VERSION = "2.0.1.dev3";
 
