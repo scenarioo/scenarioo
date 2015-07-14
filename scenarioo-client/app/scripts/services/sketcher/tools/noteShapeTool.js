4b773dcce1ce31c173275c13ee7a085ac165d669
parabolika66 <marlene.wittwer@students.fhnw.ch>
2015-07-14 19:42:39 +0200

    eslint fix

--- scenarioo-client/app/scripts/services/sketcher/tools/noteShapeTool.js
+++ scenarioo-client/app/scripts/services/sketcher/tools/noteShapeTool.js
@@ -14,6 +14,7 @@
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+/* eslint no-console:0*/
 
 
 angular.module('scenarioo.controllers').factory('NoteShapeTool', function (AbstractShapeTool) {
