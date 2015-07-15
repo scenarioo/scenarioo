138b2a0602b3be08d1868dcfe495bdf150304246
Mark Zeman <myrion211@gmail.com>
2015-07-15 19:32:45 +0200

    Added some e2e testing of the editor feature

--- scenarioo-client/test/protractorE2E/webPages/editorPage.js
+++ scenarioo-client/test/protractorE2E/webPages/editorPage.js
@@ -1,3 +1,34 @@
-/**
- * Created by Mark on 08/05/2015.
- */
+'use strict';
+
+var BaseWebPage = require('./baseWebPage.js'),
+    util = require('util'),
+    e2eUtils = require('../util/util.js');
+
+function EditorPage(overridePath) {
+    if (overridePath && overridePath.length > 0) {
+        BaseWebPage.call(this, overridePath);
+    } else {
+        BaseWebPage.call(this, '/');
+    }
+
+    this.saveButton = element(by.id('save'));
+    this.issueSaved = element(by.css('.alert'));
+
+}
+
+util.inherits(EditorPage, BaseWebPage);
+
+
+EditorPage.prototype.assertPageIsDisplayed = function () {
+    expect(this.saveButton.isDisplayed()).toBe(true);
+};
+
+EditorPage.prototype.assertIssueSaved = function() {
+    expect(this.issueSaved.isDisplayed()).toBe(true);
+};
+
+EditorPage.prototype.assertIssueNotSaved = function() {
+    expect(this.issueSaved.isDisplayed()).toBe(false);
+};
+
+module.exports = EditorPage;
