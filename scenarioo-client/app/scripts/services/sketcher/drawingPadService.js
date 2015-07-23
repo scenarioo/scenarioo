09c6fc505a5d8847cde2231a03cb15db1e8ce266
parabolika66 <marlene@bitter-sweet.ch>
2015-07-23 13:14:04 +0200

    #130 updated export function to new drawingPad structure

--- scenarioo-client/app/scripts/services/sketcher/drawingPadService.js
+++ scenarioo-client/app/scripts/services/sketcher/drawingPadService.js
@@ -217,7 +217,16 @@ angular.module('scenarioo.services').service('DrawingPadService', function ($roo
         },
 
         exportDrawing: function () {
-            return drawingPad.svg();
+            var group = drawingPad.group.clone();
+            var svg = group.first();
+            svg.attr({
+                xmlns: 'http://www.w3.org/2000/svg',
+                version: '1.1',
+                'xmlns:xlink': 'http://www.w3.org/1999/xlink',
+                width: svg.first().width(),
+                height: svg.first().height()
+            });
+            return svg.svg();
         },
 
         unSelectAllShapes: function () {
