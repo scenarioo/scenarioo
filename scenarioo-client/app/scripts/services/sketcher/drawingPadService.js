aaae28a5fb7e6a58c4d3a481b56796ebfe4d28f5
parabolika66 <marlene.wittwer@students.fhnw.ch>
2015-07-28 18:05:22 +0200

    #123 Remove namespace attributes in export functions (namespaces will be handled in backend from now on)

--- scenarioo-client/app/scripts/services/sketcher/drawingPadService.js
+++ scenarioo-client/app/scripts/services/sketcher/drawingPadService.js
@@ -214,9 +214,12 @@ angular.module('scenarioo.services').service('DrawingPadService', function ($roo
             var group = drawingPad.group.clone().hide();
             var svg = group.first();
             svg.attr({
-                xmlns: 'http://www.w3.org/2000/svg',
+                /*xmlns: 'http://www.w3.org/2000/svg',
                 version: '1.1',
                 'xmlns:xlink': 'http://www.w3.org/1999/xlink',
+                'xmlns:NS1': 'http://www.w3.org/2000/svg',
+                version: '1.1',
+                'NS1:xmlns:xlink': 'http://www.w3.org/1999/xlink',*/
                 width: svg.first().width(),
                 height: svg.first().height()
             });
