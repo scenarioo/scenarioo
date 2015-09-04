03ae85aceede65f65a5488eaee98521ac143df4c
adiherzog <adrian.herzog@gmail.com>
2015-09-04 14:32:34 +0200

    #414 Use new JS-API which supports descriptions. Add descriptions for use cases and scenarios.

--- scenarioo-client/test/protractorE2E/specs/application_startup.js
+++ scenarioo-client/test/protractorE2E/specs/application_startup.js
@@ -3,11 +3,11 @@
 var scenarioo = require('scenarioo-js');
 var pages = require('./../webPages');
 
-scenarioo.describeUseCase('Application startup', function () {
+scenarioo.describeUseCase('Application startup', 'The user is presented with the about dialog on his first Scenarioo visit.', function () {
 
     var homePage = new pages.homePage();
 
-    scenarioo.describeScenario('About dialog open on first access to Scenarioo to welcome new user', function () {
+    scenarioo.describeScenario('First visit', 'About dialog open on first access to Scenarioo to welcome new user.', function () {
         homePage.startScenariooFirstTimeVisit();
         scenarioo.docuWriter.saveStep('About dialog is displayed on first access of Scenarioo');
         homePage.assertPageIsDisplayed();
@@ -17,7 +17,7 @@ scenarioo.describeUseCase('Application startup', function () {
         scenarioo.docuWriter.saveStep('About dialog is closed');
     });
 
-    scenarioo.describeScenario('About dialog not open when previously visited', function () {
+    scenarioo.describeScenario('Later visits', 'About dialog not open when previously visited.', function () {
         homePage.startScenariooRevisited();
         scenarioo.docuWriter.saveStep('About dialog not visible for previous visitors');
         homePage.assertPageIsDisplayed();
