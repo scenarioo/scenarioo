89ee0a833209c1c9f9a846b7ffae32b8f4cf5708
adiherzog <adrian.herzog@gmail.com>
2016-04-08 07:44:14 +0200

    Merge branch 'release-2.2' into develop
    Conflicts:
    	scenarioo-client/test/protractorE2E/specs/screen_annotations.js

--- scenarioo-client/test/protractorE2E/specs/screen_annotations.js
+++ scenarioo-client/test/protractorE2E/specs/screen_annotations.js
@@ -3,7 +3,9 @@
 var scenarioo = require('scenarioo-js');
 var pages = require('./../webPages');
 
-scenarioo.describeUseCase('View screen annotations', 'Screens can have visual annotations that e.g. mark where a click was done or where data was entered.', function () {
+describeUseCaseE('View screen annotations', {
+    description: 'Screens can have visual annotations that e.g. mark where a click was done or where data was entered.'
+}, function () {
 
     var stepPage = new pages.stepPage();
 
@@ -11,40 +13,46 @@ scenarioo.describeUseCase('View screen annotations', 'Screens can have visual an
         new pages.homePage().initLocalStorage();
     });
 
-    scenarioo.describeScenario('Screen without annotations', 'No screen annotations are shown if the step does not contain any annotations', function () {
+    describeScenarioE('Screen without annotations', {
+        description: 'No screen annotations are shown if the step does not contain any annotations'
+    }, function () {
         stepPage.goToPage('/step/Find Page/find_multiple_results/startSearch.jsp/0/0');
         stepPage.assertScreenshotIsShown();
         stepPage.assertNoScreenAnnotationsArePresent();
-        scenarioo.docuWriter.saveStep('No screen annotations are shown');
+        scenarioo.saveStep('No screen annotations are shown');
     });
 
-    scenarioo.describeScenario('Screen with annotations', 'Screen annotations are shown if the step has some', function () {
+    describeScenarioE('Screen with annotations', {
+        description: 'Screen annotations are shown if the step has some'
+    }, function () {
         stepPage.goToPage('/step/Find Page/find_multiple_results/startSearch.jsp/0/1');
         stepPage.assertScreenshotIsShown();
         stepPage.assertNumberOfVisibleScreenAnnotationsIs(2);
-        scenarioo.docuWriter.saveStep('Two screen annotations are shown');
+        scenarioo.saveStep('Two screen annotations are shown');
     });
 
-    scenarioo.describeScenario('Show and hide annotations', 'Screen annotations can be hidden by clicking a button', function () {
+    describeScenarioE('Show and hide annotations', 'Screen annotations can be hidden by clicking a button', function () {
         stepPage.goToPage('/step/Find Page/find_multiple_results/startSearch.jsp/0/1');
         stepPage.assertScreenshotIsShown();
         stepPage.assertNumberOfVisibleScreenAnnotationsIs(2);
-        scenarioo.docuWriter.saveStep('Two screen annotations are shown');
+        scenarioo.saveStep('Two screen annotations are shown');
 
         stepPage.clickShowScreenAnnotationsButton();
         stepPage.assertNoScreenAnnotationsAreVisible();
-        scenarioo.docuWriter.saveStep('Screen annotations are hidden');
+        scenarioo.saveStep('Screen annotations are hidden');
 
         stepPage.clickShowScreenAnnotationsButton();
         stepPage.assertNumberOfVisibleScreenAnnotationsIs(2);
-        scenarioo.docuWriter.saveStep('Two screen annotations are shown');
+        scenarioo.saveStep('Two screen annotations are shown');
     });
 
-    scenarioo.describeScenario('Popup', 'Further information about the annotation is displayed in a popup', function() {
+    describeScenarioE('Popup', {
+        description: 'Further information about the annotation is displayed in a popup'
+    }, function() {
         stepPage.goToPage('/step/Technical%20Corner%20Cases/dummy_scenario_with_screen_annotations_of_all_types_on_one_page/specialPageWithOnlyOneVariant.jsp/0/1');
         stepPage.assertScreenshotIsShown();
         stepPage.assertNumberOfVisibleScreenAnnotationsIs(11);
-        scenarioo.docuWriter.saveStep('Eleven screen annotations are shown');
+        scenarioo.saveStep('Eleven screen annotations are shown');
 
         // This part of the test always fails on the CI but runns locally
         // TODO #473 Fix this
