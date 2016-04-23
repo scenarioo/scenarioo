94197d00df5cb99d347be27fa4870b57099040f2
Rolf Bruderer <bruderer@computerscience.ch>
2016-04-23 09:12:39 +0200

    #495 Upgrade to ScenariooJS 2.2 RC new Fluent DSL Cleaned up all e2e tests to use the new ScenariooJS fluent DSL

--- scenarioo-client/test/protractorE2E/specs/configure_label_colors.js
+++ scenarioo-client/test/protractorE2E/specs/configure_label_colors.js
@@ -3,40 +3,43 @@
 var scenarioo = require('scenarioo-js');
 var pages = require('./../webPages');
 
-describeUseCaseE('Configure label colors', {
-    description: 'Each label string can be configured to be displayed in a certain color.'
-}, function () {
+useCase('Configure label colors')
+    .description('Each label string can be configured to be displayed in a certain color.')
+    .describe(function () {
 
-    var labelConfigurationsPage = new pages.labelConfigurationsPage();
-    var homePage = new pages.homePage();
+        var labelConfigurationsPage = new pages.labelConfigurationsPage();
+        var homePage = new pages.homePage();
 
-    beforeEach(function(){
-        new pages.homePage().initLocalStorage();
+        beforeEach(function () {
+            new pages.homePage().initLocalStorage();
+        });
+
+        scenario('Create, edit and delete label configurations')
+            .it(function () {
+
+                labelConfigurationsPage.goToPage();
+                step('show label configurations');
+
+                labelConfigurationsPage.assertNumConfigurations(0);
+
+                labelConfigurationsPage.addLabelConfiguration('corner-case', 5);
+                step('add label configuration');
+
+                homePage.goToPage();
+                step('navigate away from the label config page to some other page');
+
+                labelConfigurationsPage.goToPage();
+                labelConfigurationsPage.assertNumConfigurations(1);
+                step('go back to label config page, label is still there');
+
+                labelConfigurationsPage.updateLabelConfiguration(0, 'updated', 4);
+                step('update label configuration');
+
+                labelConfigurationsPage.deleteLabelConfiguration(0);
+
+                labelConfigurationsPage.goToPage();
+                labelConfigurationsPage.assertNumConfigurations(0);
+
+            });
+
     });
-
-    describeScenarioE('Create, edit and delete label configurations', function () {
-        labelConfigurationsPage.goToPage();
-        scenarioo.saveStep('show label configurations');
-
-        labelConfigurationsPage.assertNumConfigurations(0);
-
-        labelConfigurationsPage.addLabelConfiguration('corner-case', 5);
-        scenarioo.saveStep('add label configuration');
-
-        homePage.goToPage();
-        scenarioo.saveStep('navigate away from the label config page to some other page');
-
-        labelConfigurationsPage.goToPage();
-        labelConfigurationsPage.assertNumConfigurations(1);
-        scenarioo.saveStep('go back to label config page, label is still there');
-
-        labelConfigurationsPage.updateLabelConfiguration(0, 'updated', 4);
-        scenarioo.saveStep('update label configuration');
-
-        labelConfigurationsPage.deleteLabelConfiguration(0);
-
-        labelConfigurationsPage.goToPage();
-        labelConfigurationsPage.assertNumConfigurations(0);
-    });
-
-});
--- scenarioo-client/test/protractorE2E/specs/screen_annotations.js
+++ scenarioo-client/test/protractorE2E/specs/screen_annotations.js
@@ -3,65 +3,67 @@
 var scenarioo = require('scenarioo-js');
 var pages = require('./../webPages');
 
-describeUseCaseE('View screen annotations', {
-    description: 'Screens can have visual annotations that e.g. mark where a click was done or where data was entered.'
-}, function () {
+useCase('View screen annotations')
+    .description('Screens can have visual annotations that e.g. mark where a click was done or where data was entered.')
+    .describe(function () {
 
-    var stepPage = new pages.stepPage();
+        var stepPage = new pages.stepPage();
 
-    beforeEach(function () {
-        new pages.homePage().initLocalStorage();
+        beforeEach(function () {
+            new pages.homePage().initLocalStorage();
+        });
+
+        scenario('Screen without annotations')
+            .description('No screen annotations are shown if the step does not contain any annotations')
+            .it(function () {
+                stepPage.goToPage('/step/Find Page/find_multiple_results/startSearch.jsp/0/0');
+                stepPage.assertScreenshotIsShown();
+                stepPage.assertNoScreenAnnotationsArePresent();
+                step('No screen annotations are shown');
+            });
+
+        scenario('Screen with annotations')
+            .description('Screen annotations are shown if the step has some')
+            .it(function () {
+                stepPage.goToPage('/step/Find Page/find_multiple_results/startSearch.jsp/0/1');
+                stepPage.assertScreenshotIsShown();
+                stepPage.assertNumberOfVisibleScreenAnnotationsIs(2);
+                step('Two screen annotations are shown');
+            });
+
+        scenario('Show and hide annotations')
+            .description('Screen annotations can be hidden by clicking a button')
+            .it(function () {
+                stepPage.goToPage('/step/Find Page/find_multiple_results/startSearch.jsp/0/1');
+                stepPage.assertScreenshotIsShown();
+                stepPage.assertNumberOfVisibleScreenAnnotationsIs(2);
+                step('Two screen annotations are shown');
+
+                stepPage.clickShowScreenAnnotationsButton();
+                stepPage.assertNoScreenAnnotationsAreVisible();
+                step('Screen annotations are hidden');
+
+                stepPage.clickShowScreenAnnotationsButton();
+                stepPage.assertNumberOfVisibleScreenAnnotationsIs(2);
+                step('Two screen annotations are shown');
+            });
+
+        scenario('Popup')
+            .description('Further information about the annotation is displayed in a popup')
+            .it(function () {
+                stepPage.goToPage('/step/Technical%20Corner%20Cases/dummy_scenario_with_screen_annotations_of_all_types_on_one_page/specialPageWithOnlyOneVariant.jsp/0/1');
+                stepPage.assertScreenshotIsShown();
+                stepPage.assertNumberOfVisibleScreenAnnotationsIs(11);
+                step('Eleven screen annotations are shown');
+
+                // This part of the test always fails on the CI but runns locally
+                // TODO #473 Fix this
+                /*
+                 stepPage.clickFirstScreenAnnotation();
+                 stepPage.assertScreenAnnotationPopupIsDisplayed();
+                 stepPage.assertTitleOfAnnotationPopupIs('DEFAULT-Annotation \'Life Is Beautiful\'');
+                 scenarioo.docuWriter.saveStep('Popup is shown');
+                 */
+            });
+
     });
-
-    describeScenarioE('Screen without annotations', {
-        description: 'No screen annotations are shown if the step does not contain any annotations'
-    }, function () {
-        stepPage.goToPage('/step/Find Page/find_multiple_results/startSearch.jsp/0/0');
-        stepPage.assertScreenshotIsShown();
-        stepPage.assertNoScreenAnnotationsArePresent();
-        scenarioo.saveStep('No screen annotations are shown');
-    });
-
-    describeScenarioE('Screen with annotations', {
-        description: 'Screen annotations are shown if the step has some'
-    }, function () {
-        stepPage.goToPage('/step/Find Page/find_multiple_results/startSearch.jsp/0/1');
-        stepPage.assertScreenshotIsShown();
-        stepPage.assertNumberOfVisibleScreenAnnotationsIs(2);
-        scenarioo.saveStep('Two screen annotations are shown');
-    });
-
-    describeScenarioE('Show and hide annotations', 'Screen annotations can be hidden by clicking a button', function () {
-        stepPage.goToPage('/step/Find Page/find_multiple_results/startSearch.jsp/0/1');
-        stepPage.assertScreenshotIsShown();
-        stepPage.assertNumberOfVisibleScreenAnnotationsIs(2);
-        scenarioo.saveStep('Two screen annotations are shown');
-
-        stepPage.clickShowScreenAnnotationsButton();
-        stepPage.assertNoScreenAnnotationsAreVisible();
-        scenarioo.saveStep('Screen annotations are hidden');
-
-        stepPage.clickShowScreenAnnotationsButton();
-        stepPage.assertNumberOfVisibleScreenAnnotationsIs(2);
-        scenarioo.saveStep('Two screen annotations are shown');
-    });
-
-    describeScenarioE('Popup', {
-        description: 'Further information about the annotation is displayed in a popup'
-    }, function() {
-        stepPage.goToPage('/step/Technical%20Corner%20Cases/dummy_scenario_with_screen_annotations_of_all_types_on_one_page/specialPageWithOnlyOneVariant.jsp/0/1');
-        stepPage.assertScreenshotIsShown();
-        stepPage.assertNumberOfVisibleScreenAnnotationsIs(11);
-        scenarioo.saveStep('Eleven screen annotations are shown');
-
-        // This part of the test always fails on the CI but runns locally
-        // TODO #473 Fix this
-        /*
-        stepPage.clickFirstScreenAnnotation();
-        stepPage.assertScreenAnnotationPopupIsDisplayed();
-        stepPage.assertTitleOfAnnotationPopupIs('DEFAULT-Annotation \'Life Is Beautiful\'');
-        scenarioo.docuWriter.saveStep('Popup is shown');
-        */
-    });
-
-});
