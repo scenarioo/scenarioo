9dd8f0396da1fe12a0ab2e937dd3df21b99998a6
Pascal Forster <pforster@hsr.ch>
2016-04-25 12:40:37 +0200

    scenarioo/scenarioo#492: added missing javadoc

--- scenarioo-server/src/main/java/org/scenarioo/model/diffViewer/BuildDiffInfo.java
+++ scenarioo-server/src/main/java/org/scenarioo/model/diffViewer/BuildDiffInfo.java
@@ -25,7 +25,7 @@ import javax.xml.bind.annotation.XmlSeeAlso;
 import org.scenarioo.model.docu.entities.UseCase;
 
 /**
- * 
+ * Contains the diff information for a Build.
  */
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
