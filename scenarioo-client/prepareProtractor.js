39076e2d5089a29f3289dc97fcf49afa2af9b162
Rolf Bruderer <bruderer@computerscience.ch>
2016-04-28 14:25:50 +0200

    #496 E2E Tests: turn off animations

--- scenarioo-client/prepareProtractor.js
+++ scenarioo-client/prepareProtractor.js
@@ -8,6 +8,7 @@ function prepareProtractorForE2ETests() {
     setupJasmineXmlReporters();
     setupScenariooFluentDsl();
     configureBrowserWindowSize();
+    disableAnimations();
 
     /**
      * Jasmine XML Reporters are needed to see test failures in nice report on jenkins.
@@ -38,6 +39,38 @@ function prepareProtractorForE2ETests() {
         var width = 1280;
         var height = 800;
         browser.driver.manage().window().setSize(width, height);
+    }
+
+    function disableAnimations() {
+
+        var disableNgAnimate = function () {
+            angular
+                .module('disableNgAnimate', [])
+                .run(['$animate', function ($animate) {
+                    $animate.enabled(false);
+                }]);
+        };
+
+        var disableCssAnimate = function () {
+            angular
+                .module('disableCssAnimate', [])
+                .run(function () {
+                    var style = document.createElement('style');
+                    style.type = 'text/css';
+                    style.innerHTML = '* {' +
+                        '-webkit-transition: none !important;' +
+                        '-moz-transition: none !important' +
+                        '-o-transition: none !important' +
+                        '-ms-transition: none !important' +
+                        'transition: none !important' +
+                        '}';
+                    document.getElementsByTagName('head')[0].appendChild(style);
+                });
+        };
+
+        browser.addMockModule('disableNgAnimate', disableNgAnimate);
+        browser.addMockModule('disableCssAnimate', disableCssAnimate);
+
     }
 
 }
