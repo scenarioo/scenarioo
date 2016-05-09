d9be139320a56524cf693372ffee53e6c4d46c74
adiherzog <adrian.herzog@gmail.com>
2016-05-09 17:14:51 +0200

    #429 Generate environment_config.js in correct folder and ignore it for git

--- scenarioo-client/app/environment_config.js
+++
@@ -1,5 +0,0 @@
-// This file is written by a gulp task. Configuration is done in environments.json
-
-angular.module('scenarioo.config', [])
-.constant('BASE_URL', 'http://localhost:8080/scenarioo/')
-.constant('ENV', 'development');
