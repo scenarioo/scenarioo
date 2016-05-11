5b724ce80fa4325974cfb8f85152923fa8f8875f
mi-we <michael.werner@windowslive.com>
2016-05-11 10:57:28 +0200

    #509 Refactored controller and view for Use Case Tab.

--- scenarioo-client/app/build/useCasesTab.controller.js
+++ scenarioo-client/app/build/useCasesTab.controller.js
@@ -15,75 +15,92 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-angular.module('scenarioo.controllers').controller('UseCasesTabController', function ($scope, $location, $filter, GlobalHotkeysService, BranchesAndBuildsService, SelectedBranchAndBuildService, UseCasesResource, LabelConfigurationsResource) {
+angular.module('scenarioo.controllers').controller('UseCasesTabController', UseCasesTabController);
 
-  var transformMetadataToTree = $filter('scMetadataTreeCreator');
-  var transformMetadataToTreeArray = $filter('scMetadataTreeListCreator');
-  var dateTimeFormatter = $filter('scDateTime');
-  SelectedBranchAndBuildService.callOnSelectionChange(loadUseCases);
+UseCasesTabController.$inject = ['$location', '$filter', 'BranchesAndBuildsService', 'SelectedBranchAndBuildService', 'UseCasesResource', 'LabelConfigurationsResource'];
+function UseCasesTabController($location, $filter, BranchesAndBuildsService, SelectedBranchAndBuildService, UseCasesResource, LabelConfigurationsResource) {
+    var vm = this;
+    var transformMetadataToTree = $filter('scMetadataTreeCreator');
+    var transformMetadataToTreeArray = $filter('scMetadataTreeListCreator');
+    var dateTimeFormatter = $filter('scDateTime');
 
-  // FIXME this code is duplicated. How can we extract it into a service?
-  LabelConfigurationsResource.query({}, function (labelConfiguratins) {
-    $scope.labelConfigurations = labelConfiguratins;
-  });
+    vm.table = {
+        search: {searchTerm: ''},
+        sort: {column: 'name', reverse: false}
+    };
+    vm.goToUseCase = goToUseCase;
+    vm.onNavigatorTableHit = onNavigatorTableHit;
+    vm.resetSearchField = resetSearchField;
 
-  function loadUseCases(selected) {
+    // FIXME this code is duplicated. How can we extract it into a service?
+    vm.getLabelStyle = getLabelStyle;
 
-    BranchesAndBuildsService.getBranchesAndBuildsService()
-      .then(function onSuccess(branchesAndBuilds) {
-        $scope.branchesAndBuilds = branchesAndBuilds;
-      }).then(function () {
-        UseCasesResource.query(
-          {'branchName': selected.branch, 'buildName': selected.build},
-          function onSuccess(result) {
-            $scope.useCases = result;
+    activate();
 
-            var branch = $scope.branchesAndBuilds.selectedBranch.branch;
-            var build = $scope.branchesAndBuilds.selectedBuild.build;
-            $scope.branchInformationTree = createBranchInformationTree(branch);
-            $scope.buildInformationTree = createBuildInformationTree(build);
-            $scope.metadataTreeBranches = transformMetadataToTreeArray(branch.details);
-            $scope.metadataTreeBuilds = transformMetadataToTreeArray(build.details);
-          });
-      });
-
-  }
-
-  $scope.goToUseCase = function (useCaseName) {
-    $location.path('/usecase/' + useCaseName);
-  };
-
-  $scope.onNavigatorTableHit = function (useCase) {
-    $scope.goToUseCase(useCase.name);
-  };
-
-  $scope.table = {search: {searchTerm: ''}, sort: {column: 'name', reverse: false}};
-
-  $scope.resetSearchField = function () {
-    $scope.table.search = {searchTerm: ''};
-  };
-
-  function createBranchInformationTree(branch) {
-    var branchInformationTree = {};
-    branchInformationTree.Description = branch.description;
-    return transformMetadataToTree(branchInformationTree);
-  }
-
-  function createBuildInformationTree(build) {
-    var buildInformationTree = {};
-    buildInformationTree.Date = dateTimeFormatter(build.date);
-    buildInformationTree.Revision = build.revision;
-    buildInformationTree.Status = build.status;
-    return transformMetadataToTree(buildInformationTree);
-  }
-
-  // FIXME this code is duplicated. How can we extract it into a service?
-  $scope.getLabelStyle = function (labelName) {
-    if ($scope.labelConfigurations) {
-      var labelConfig = $scope.labelConfigurations[labelName];
-      if (labelConfig) {
-        return {'background-color': labelConfig.backgroundColor, 'color': labelConfig.foregroundColor};
-      }
+    function goToUseCase(useCaseName) {
+        $location.path('/usecase/' + useCaseName);
     }
-  };
-});
+
+    function onNavigatorTableHit(useCase) {
+        vm.goToUseCase(useCase.name);
+    }
+
+    function resetSearchField() {
+        vm.table.search = {searchTerm: ''};
+    }
+
+    function getLabelStyle(labelName) {
+        if (vm.labelConfigurations) {
+            var labelConfig = vm.labelConfigurations[labelName];
+            if (labelConfig) {
+                return {'background-color': labelConfig.backgroundColor, 'color': labelConfig.foregroundColor};
+            }
+        }
+    }
+
+    function activate() {
+        SelectedBranchAndBuildService.callOnSelectionChange(loadUseCases);
+
+        // FIXME this code is duplicated. How can we extract it into a service?
+        LabelConfigurationsResource.query({}, function (labelConfiguratins) {
+            vm.labelConfigurations = labelConfiguratins;
+        });
+    }
+
+    function loadUseCases(selected) {
+
+        BranchesAndBuildsService.getBranchesAndBuildsService()
+            .then(function onSuccess(branchesAndBuilds) {
+                vm.branchesAndBuilds = branchesAndBuilds;
+            }).then(function () {
+            UseCasesResource.query(
+                {'branchName': selected.branch, 'buildName': selected.build},
+                function onSuccess(result) {
+                    vm.useCases = result;
+
+                    var branch = vm.branchesAndBuilds.selectedBranch.branch;
+                    var build = vm.branchesAndBuilds.selectedBuild.build;
+                    vm.branchInformationTree = createBranchInformationTree(branch);
+                    vm.buildInformationTree = createBuildInformationTree(build);
+                    vm.metadataTreeBranches = transformMetadataToTreeArray(branch.details);
+                    vm.metadataTreeBuilds = transformMetadataToTreeArray(build.details);
+                });
+        });
+
+    }
+
+    function createBranchInformationTree(branch) {
+        var branchInformationTree = {};
+        branchInformationTree.Description = branch.description;
+        return transformMetadataToTree(branchInformationTree);
+    }
+
+    function createBuildInformationTree(build) {
+        var buildInformationTree = {};
+        buildInformationTree.Date = dateTimeFormatter(build.date);
+        buildInformationTree.Revision = build.revision;
+        buildInformationTree.Status = build.status;
+        return transformMetadataToTree(buildInformationTree);
+    }
+
+}
