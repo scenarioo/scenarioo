0302a814c6944386b0199328a4c365478a099da0
danielsuter <suter_daniel@hotmail.com>
2016-10-17 18:52:53 +0200

    #541 Little hack to avoid massive logging.

--- scenarioo-server/src/test/java/org/scenarioo/business/diffViewer/comparator/ScreenshotComparatorGraphicsMagickTest.java
+++ scenarioo-server/src/test/java/org/scenarioo/business/diffViewer/comparator/ScreenshotComparatorGraphicsMagickTest.java
@@ -1,9 +1,12 @@
 package org.scenarioo.business.diffViewer.comparator;
 
+import org.apache.log4j.Appender;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.varia.NullAppender;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,8 +18,7 @@ import org.scenarioo.utils.TestFileUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -46,6 +48,23 @@ public class ScreenshotComparatorGraphicsMagickTest {
 		RepositoryLocator.INSTANCE.getConfigurationRepository().updateConfiguration(getTestConfiguration());
 		screenshotComparator = new ScreenshotComparator(BASE_BRANCH_NAME, BASE_BUILD_NAME,
 			getComparisonConfiguration());
+	}
+
+	// Avoids massive logging which is expected
+	private Enumeration allAppenders;
+
+	@Before
+	public void removeAppenders() {
+		allAppenders = Logger.getRootLogger().getAllAppenders();
+		Logger.getRootLogger().removeAllAppenders();
+		Logger.getRootLogger().addAppender(new NullAppender());
+	}
+
+	@After
+	public void restoreAppenders() {
+		for (Object appender : Collections.list(allAppenders)) {
+			Logger.getRootLogger().addAppender((Appender) appender);
+		}
 	}
 
 	@Test
@@ -82,8 +101,8 @@ public class ScreenshotComparatorGraphicsMagickTest {
 
 	@Test
 	public void compareNonExistentScreenshots() {
-		final Logger LOGGER = screenshotComparator.getLogger();
-		final TestAppender appender = new TestAppender();
+		Logger LOGGER = ScreenshotComparator.getLogger();
+		TestAppender appender = new TestAppender();
 		LOGGER.addAppender(appender);
 
 		final double difference = screenshotComparator.compareScreenshots(BASE_SCREENSHOT,
