d37be6eea631935b26b9855dc3c15d594aa81e7f
danielsuter <suter_daniel@hotmail.com>
2016-10-20 20:08:11 +0200

    #541 As suggested by review. Log msg once for debugging purposes.

--- scenarioo-server/src/main/java/org/scenarioo/dao/diffViewer/GraphicsMagickConfiguration.java
+++ scenarioo-server/src/main/java/org/scenarioo/dao/diffViewer/GraphicsMagickConfiguration.java
@@ -1,13 +1,17 @@
 package org.scenarioo.dao.diffViewer;
 
+import org.apache.log4j.Logger;
 import org.im4java.core.CompareCmd;
 import org.im4java.core.IM4JavaException;
 import org.im4java.core.IMOperation;
 import org.im4java.process.ArrayListOutputConsumer;
+import org.scenarioo.dao.basic.FileSystemOperationsDao;
 
 import java.io.IOException;
 
 public class GraphicsMagickConfiguration {
+	private static final Logger LOGGER = Logger.getLogger(GraphicsMagickConfiguration.class);
+
 	private static Boolean isAvailable;
 
 	public static synchronized boolean isAvailable() {
@@ -24,10 +28,13 @@ public class GraphicsMagickConfiguration {
 		try {
 			gmConsole.run(gmOperation.version());
 		} catch (IOException e) {
+			LOGGER.warn(e, e);
 			return false;
 		} catch (InterruptedException e) {
+			LOGGER.warn(e, e);
 			return false;
 		} catch (IM4JavaException e) {
+			LOGGER.warn(e, e);
 			return false;
 		}
 		return true;
