3ef4ba6b4304818c3ce0bdc00174b38303acff61
Tobias Schmitz <tschmitz@hsr.ch>
2017-03-27 15:48:04 +0200

    site nav

---
+++ scenarioo-client/app/dashboard/comp/site-navigation/site-navigation.js
@@ -0,0 +1,31 @@
+function SiteNavigationController($location){
+    var site = this;
+    site.links = [
+        {
+            name: 'detailNav',
+            short: 'D'
+        },{
+            name: 'dashboard',
+            short: 'S'
+        },{
+            name: 'accordion',
+            short: 'A'
+        },
+    ];
+
+    site.setView = function(viewName) {
+
+        console.log("change view to "+viewName);
+        $location.path('/'+viewName+'/');
+    };
+
+}
+
+
+angular.module('scenarioo').component('siteNavigation', {
+    templateUrl: 'dashboard/comp/site-navigation/site-navigation.html',
+    bindings: {
+        current: '@'
+    },
+    controller: SiteNavigationController
+});
