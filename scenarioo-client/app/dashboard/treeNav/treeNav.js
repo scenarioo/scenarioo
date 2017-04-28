2ec582fe5e1bc347fef5e361f4ec234d93e07465
Phil <waldi_92@hotmail.com>
2017-04-28 12:55:22 +0200

    pageslide for tree navigation

--- scenarioo-client/app/dashboard/treeNav/treeNav.js
+++ scenarioo-client/app/dashboard/treeNav/treeNav.js
@@ -2,5 +2,7 @@ angular.module('scenarioo').component('treeNav', {
     templateUrl: 'dashboard/treeNav/treeNav.html',
     controllerAs: 'treeNav',
     bindings: {
+        rootFeature:'=',
+        clickFeature:'='
     }
 });
