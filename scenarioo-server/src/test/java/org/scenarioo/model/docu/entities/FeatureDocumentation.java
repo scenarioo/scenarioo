ffaf92f8da30d13ca60c6827baefa8d52d58f5a3
Phil <waldi_92@hotmail.com>
2017-05-05 09:48:13 +0200

    agile project dashboard with milestones and status

--- scenarioo-server/src/test/java/org/scenarioo/model/docu/entities/FeatureDocumentation.java
+++ scenarioo-server/src/test/java/org/scenarioo/model/docu/entities/FeatureDocumentation.java
@@ -18,6 +18,8 @@ public class FeatureDocumentation {
 		feature.name = "Dashboard-View";
 		feature.description = "Dashboard mit Subfeature und subsubFeature Cards, welche sortiert und mit swimlanes geordnet werden können";
 		feature.folderName = "dashboard-view";
+		feature.status = Status.FAILED.getKeyword();
+		feature.milestone = "R3";
 		writeToFile(feature);
 		Assert.assertTrue(new File(baseFolder + "/" + feature.folderName + "/feature.xml").exists());
 	}
@@ -29,6 +31,8 @@ public class FeatureDocumentation {
 		feature.name = "Feature-View";
 		feature.description = "Overview over the current subfeatures, compareable to todays usecase view";
 		feature.folderName = "feature-view";
+		feature.status = Status.FAILED.getKeyword();
+		feature.milestone = "R1";
 		writeToFile(feature);
 		Assert.assertTrue(new File(baseFolder + "/" + feature.folderName + "/feature.xml").exists());
 	}
@@ -40,6 +44,8 @@ public class FeatureDocumentation {
 		feature.name = "Scenario-View";
 		feature.description = "Overview over the current scenarios, comparable to todays scenario view";
 		feature.folderName = "scenario-view";
+		feature.status = Status.FAILED.getKeyword();
+		feature.milestone = "R1";
 		writeToFile(feature);
 		Assert.assertTrue(new File(baseFolder + "/" + feature.folderName + "/feature.xml").exists());
 	}
@@ -51,6 +57,8 @@ public class FeatureDocumentation {
 		feature.name = "Documentation-View";
 		feature.description = "Shows the documentation file and the spec file of a feature and the subfeatures with their files";
 		feature.folderName = "documentation-view";
+		feature.status = Status.FAILED.getKeyword();
+		feature.milestone = "R1";
 		writeToFile(feature);
 		Assert.assertTrue(new File(baseFolder + "/" + feature.folderName + "/feature.xml").exists());
 	}
@@ -69,6 +77,7 @@ public class FeatureDocumentation {
 		feature.featureNames.add("dashboard-view");
 		feature.featureNames.add("tree-navigation");
 		feature.status = Status.SUCCESS.getKeyword();
+		feature.milestone = "R1";
 		writeToFile(feature);
 		Assert.assertTrue(new File(baseFolder + "/" + feature.folderName + "/feature.xml").exists());
 	}
@@ -80,6 +89,8 @@ public class FeatureDocumentation {
 		feature.name = "Importer";
 		feature.description = "Imports the data to save it on the server";
 		feature.folderName = "importer";
+		feature.status = Status.FAILED.getKeyword();
+		feature.milestone = "R1";
 		writeToFile(feature);
 		Assert.assertTrue(new File(baseFolder + "/" + feature.folderName + "/feature.xml").exists());
 	}
@@ -95,6 +106,8 @@ public class FeatureDocumentation {
 		feature.featureNames.add("gui-elements");
 		feature.featureNames.add("sketch-editor");
 		feature.featureNames.add("client-api-connection");
+		feature.status = Status.FAILED.getKeyword();
+		feature.milestone = "R1";
 		writeToFile(feature);
 		Assert.assertTrue(new File(baseFolder + "/" + feature.folderName + "/feature.xml").exists());
 	}
@@ -112,7 +125,8 @@ public class FeatureDocumentation {
 		feature.featureNames.add("full-text-search");
 		feature.featureNames.add("sketch-editor");
 		feature.featureNames.add("rest-api");
-		feature.status = Status.SUCCESS.getKeyword();
+		feature.status = Status.FAILED.getKeyword();
+		feature.milestone = "R1";
 		writeToFile(feature);
 		Assert.assertTrue(new File(baseFolder + "/" + feature.folderName + "/feature.xml").exists());
 	}
@@ -124,6 +138,8 @@ public class FeatureDocumentation {
 		feature.name = "Comparison";
 		feature.description = "Contains the work to do for the comparison with older builds";
 		feature.folderName = "comparison";
+		feature.status = Status.FAILED.getKeyword();
+		feature.milestone = "R2";
 		writeToFile(feature);
 		Assert.assertTrue(new File(baseFolder + "/" + feature.folderName + "/feature.xml").exists());
 	}
@@ -135,6 +151,8 @@ public class FeatureDocumentation {
 		feature.name = "Full Text Search";
 		feature.description = "Every page must be searchable with full text search";
 		feature.folderName = "full-text-search";
+		feature.status = Status.FAILED.getKeyword();
+		feature.milestone = "R2";
 		writeToFile(feature);
 		Assert.assertTrue(new File(baseFolder + "/" + feature.folderName + "/feature.xml").exists());
 	}
@@ -146,7 +164,8 @@ public class FeatureDocumentation {
 		feature.name = "Sketch Editor";
 		feature.description = "Editor to annotate Screenshots with extra information like Textboxes, or markings";
 		feature.folderName = "sketch-editor";
-
+		feature.status = Status.FAILED.getKeyword();
+		feature.milestone = "R3";
 		writeToFile(feature);
 		Assert.assertTrue(new File(baseFolder + "/" + feature.folderName + "/feature.xml").exists());
 	}
@@ -158,7 +177,8 @@ public class FeatureDocumentation {
 		feature.name = "Featrue Tree Navigation";
 		feature.description = "navigation to other featrues in currently sellected build";
 		feature.folderName = "tree-navigation";
-
+		feature.status = Status.FAILED.getKeyword();
+		feature.milestone = "R2";
 		writeToFile(feature);
 		Assert.assertTrue(new File(baseFolder + "/" + feature.folderName + "/feature.xml").exists());
 	}
@@ -170,6 +190,8 @@ public class FeatureDocumentation {
 		feature.name = "Rest API";
 		feature.description = "All changes necessary to the Rest API";
 		feature.folderName = "rest-api";
+		feature.status = Status.FAILED.getKeyword();
+		feature.milestone = "R1";
 		writeToFile(feature);
 		Assert.assertTrue(new File(baseFolder + "/" + feature.folderName + "/feature.xml").exists());
 	}
@@ -180,6 +202,8 @@ public class FeatureDocumentation {
 		feature.name = "Client API Connection";
 		feature.description = " all changes to the client side API";
 		feature.folderName = "client-api-connection";
+		feature.status = Status.FAILED.getKeyword();
+		feature.milestone = "R1";
 		writeToFile(feature);
 		Assert.assertTrue(new File(baseFolder + "/" + feature.folderName + "/feature.xml").exists());
 	}
@@ -199,6 +223,5 @@ public class FeatureDocumentation {
 		baseFolder = new File("").getAbsolutePath() + "/docs/features/";
 		new File(baseFolder).mkdirs();
 	}
-
-
+	
 }
