1ded372af6c3ab7cc875cf4c5c01a029ca752901
Phil <waldi_92@hotmail.com>
2017-05-05 16:21:20 +0200

    change release to milestone test for dashboard

---
+++ scenarioo-client/test/spec/dashboard/dashboard.conroller.spec.js
@@ -0,0 +1,54 @@
+/* scenarioo-client
+ * Copyright (C) 2014, scenarioo.org Development Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+'use strict';
+
+describe('DashboardController', function () {
+    var $location, $scope;
+    var dashboardController;
+
+    beforeEach(module('scenarioo.controllers'));
+
+    beforeEach(inject(function ($controller, $rootScope, _$location_) {
+            $location = _$location_;
+
+            $scope = $rootScope.$new();
+            dashboardController = $controller('DashboardController', {$scope: $scope});
+        }
+    ));
+
+    it('has no features and builds set in the beginning', function () {
+        expect(dashboardController.features.length).toBe(0);
+        expect(dashboardController.branchesAndBuilds.length).toBe(0);
+    });
+
+    it('navigates to feature when link is clicked', function () {
+        expect($location.path()).toBe('');
+
+        var dummyFeature = { name: 'DisplayWeather', diffInfo: {isRemoved: false} };
+        dashboardController.clickFeature(dummyFeature, '/dashboard');
+
+        expect($location.path()).toBe('/dashboard/DisplayWeather');
+    });
+
+    it('opens treenavigation and navigates to root feature', function(){
+        expect($location.path()).toBe('/dashboard/DisplayWeather');
+        element(by.id('pageslideButton')).click();
+        element(by.id('root')).click();
+        expect($location.path()).toBe('');
+    });
+});
