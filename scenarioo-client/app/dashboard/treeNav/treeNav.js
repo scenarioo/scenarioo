2630cba9a633c8d3ec608a2bbc342eba79c5d1fd
Phil <waldi_92@hotmail.com>
2017-05-08 19:36:11 +0200

    navigation

--- scenarioo-client/app/dashboard/treeNav/treeNav.js
+++ scenarioo-client/app/dashboard/treeNav/treeNav.js
@@ -5,38 +5,9 @@ angular.module('scenarioo').component('treeNav', {
         rootFeature:'=',
         currentFeature:'=',
         clickFeature:'=',
-        size:'='
     },
     controller: function () {
-        var treeNav = this;
-
-        function getOffset(el) {
-            el = el.getBoundingClientRect();
-            return {
-                left: el.left + window.scrollX,
-                top: el.top + window.scrollY
-            }
-        }
-
-        var elem = document.getElementsByClassName('fuu')[0];
-
-        var st = 'min-width: 0px;white-space: nowrap;overflow: hidden;';
-
-        elem.setAttribute('style','width: '+ (treeNav.size) +'px; '+st);
-        localStorage.setItem('MAV_SIZE_LEFT', treeNav.size);
-
-        var resizing = false;
-        document.addEventListener('mousemove', function (event) {
-            if (resizing){
-                elem.setAttribute('style','width: '+ (event.clientX -getOffset(elem).left) +'px; '+st);
-                localStorage.setItem('MAV_SIZE_LEFT', elem.offsetWidth);
-            }
-        });
-        document.addEventListener('mouseup', function (event) {
-            resizing = false;
-        });
-        treeNav.resize = function () {
-            resizing = true;
-        }
+        var treeNav=this;
+        treeNav.showNav=true;
     }
 });
