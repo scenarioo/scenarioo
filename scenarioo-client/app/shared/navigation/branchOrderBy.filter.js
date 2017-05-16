db7a3496db77c4361b36d7d02d528435c7416171
kabl <blaser_k@gmx.ch>
2017-05-16 13:57:52 +0200

    601: shorter angular declaration

--- scenarioo-client/app/shared/navigation/branchOrderBy.filter.js
+++ scenarioo-client/app/shared/navigation/branchOrderBy.filter.js
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-angular.module('scenarioo.filters').filter('scBranchOrderBy', ['ConfigService', function(ConfigService) { return scBranchOrderByFilter(ConfigService);}]);
+angular.module('scenarioo.filters').filter('scBranchOrderBy', ['ConfigService', scBranchOrderByFilter]);
 
 function scBranchOrderByFilter(ConfigService) {
 
