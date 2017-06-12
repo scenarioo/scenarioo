f7c265d483cbbbe3fda752d7af13aa94cdacce61
Rolf Bruderer <bruderer@computerscience.ch>
2017-06-13 00:18:05 +0200

    Add dummy test to produce demo data for dashboard feature

---
+++ scenarioo-docu-generation-example/src/test/java/org/scenarioo/example/dashboard/DashboardExampleDataTest.java
@@ -0,0 +1,28 @@
+package org.scenarioo.example.dashboard;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+/**
+ * A Dummy test to cause the build to copy the test data for dashboard to the demo data directory
+ */
+public class DashboardExampleDataTest {
+
+	@Test
+	public void copyDashboardTestDataToExampleDocuDataOutputDir() throws URISyntaxException, IOException {
+		File targetDir = new File("build/scenarioDocuExample/dashboard");
+		File sourceDir = getResourceFile("example/documentation/scenarioDocuExample/dashboard");
+		FileUtils.copyDirectory(sourceDir, targetDir);
+	}
+
+	private static File getResourceFile(final String relativeResourcePath) throws URISyntaxException {
+		URL url = DashboardExampleDataTest.class.getClassLoader().getResource(relativeResourcePath);
+		return new File(url.toURI());
+	}
+
+}
