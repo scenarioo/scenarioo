284a69a86e8e3966867b592f992952491dbecdbc
Sascha Kaufmann <sascha.kaufmann92@gmail.com>
2017-10-04 10:19:11 +0200

    #427 fix workspace directory for self docu deployment

--- ci/deploySelfDocu.sh
+++ ci/deploySelfDocu.sh
@@ -19,6 +19,7 @@ done
 
 # Properties
 SCENARIOO_DATA_ROOT=/var/lib/scenarioo
+PIPELINE_BRANCH_DIR=/var/lib/jenkins/jobs/scenarioo-ci-pipeline/branches/$BRANCH
 
 # just to prevent errors because of missing archive directories
 mkdir -p $SCENARIOO_DATA_ROOT/scenarioo-self-docu-archive/$BRANCH/scenarioo-self-docu
@@ -31,7 +32,7 @@ cp -r $SCENARIOO_DATA_ROOT/scenarioo-self-docu-archive-most-recent/$BRANCH/scena
 
 #Simply copy the generated documentation data to the scenarioo docu dir
 echo "Deploy the generated self docu build"
-cd /var/lib/jenkins/jobs/scenarioo-$BRANCH/workspace/scenarioo-client
+cd $PIPELINE_BRANCH_DIR/workspace/scenarioo-client
 cp -rf scenariooDocumentation/scenarioo-self-docu $SCENARIOO_DATA_ROOT/scenarioDocuExample-$BRANCH
 rm -rf scenariooDocumentation/scenarioo-self-docu
 
