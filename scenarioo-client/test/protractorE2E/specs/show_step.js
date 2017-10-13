044dc9e5399cb54470ebc69e45f44b02e71d5c4d
Rolf Bruderer <bruderer@computerscience.ch>
2017-10-13 21:30:59 +0200

    Pipeline: just testing if failing e2e test works

--- scenarioo-client/test/protractorE2E/specs/show_step.js
+++ scenarioo-client/test/protractorE2E/specs/show_step.js
@@ -227,7 +227,7 @@ useCase('Show step')
                 step('Show added step base screenshot in single page view');
 
                 stepPage.showHighlights();
-                stepPage.assertStepComparisonSinglePageViewIsActive();
+                stepPage.assertStepComparisonSideBySideViewIsActive();
                 step('Show added step diff screenshot in single page view');
 
             });
