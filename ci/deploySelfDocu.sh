e0b1987119226186a96e6d504b6bad9a7d59fc67
Rolf Bruderer <bruderer@computerscience.ch>
2017-10-15 19:18:53 +0200

    adjust self docu deployment to new branch names

--- ci/deploySelfDocu.sh
+++ ci/deploySelfDocu.sh
@@ -23,20 +23,21 @@ echo "Workspace Dir: $WORKSPACE_DIR"
 
 # Properties
 SCENARIOO_DATA_ROOT=/var/lib/scenarioo
+SCENARIOO_DATA_DIR=/var/lib/scenarioo/scenarioDocuExample-$BRANCH
 
-# just to prevent errors because of missing archive directories
-mkdir -p $SCENARIOO_DATA_ROOT/scenarioo-self-docu-archive/$BRANCH/scenarioo-self-docu
-mkdir -p $SCENARIOO_DATA_ROOT/scenarioo-self-docu-archive-most-recent/$BRANCH/scenarioo-self-docu
+# Precreate archive directories (just to prevent errors because of missing archive directories)
+mkdir -p $SCENARIOO_DATA_ROOT/scenarioo-self-docu-archive/scenarioo-$BRANCH
+mkdir -p $SCENARIOO_DATA_ROOT/scenarioo-self-docu-archive-most-recent/scenarioo-$BRANCH
 
 # First copy all archived self docu builds back
 echo "Restoring archived self docu builds"
-cp -r $SCENARIOO_DATA_ROOT/scenarioo-self-docu-archive/$BRANCH/scenarioo-self-docu $SCENARIOO_DATA_ROOT/scenarioDocuExample-$BRANCH
-cp -r $SCENARIOO_DATA_ROOT/scenarioo-self-docu-archive-most-recent/$BRANCH/scenarioo-self-docu $SCENARIOO_DATA_ROOT/scenarioDocuExample-$BRANCH
+cp -r $SCENARIOO_DATA_ROOT/scenarioo-self-docu-archive/* $SCENARIOO_DATA_DIR
+cp -r $SCENARIOO_DATA_ROOT/scenarioo-self-docu-archive-most-recent/* $SCENARIOO_DATA_DIR
 
-#Simply copy the generated documentation data to the scenarioo docu dir
+# Deploy generated self docu of this build run
 echo "Deploy the generated self docu build"
-cp -rf ./scenarioo-client/scenariooDocumentation/scenarioo-self-docu $SCENARIOO_DATA_ROOT/scenarioDocuExample-$BRANCH
-rm -rf ./scenarioo-client/scenariooDocumentation/scenarioo-self-docu
+cp -rf ./scenarioo-client/scenariooDocumentation/* $SCENARIOO_DATA_DIR
+rm -rf ./scenarioo-client/scenariooDocumentation
 
 # Trigger scenarioo demo to import new builds
 curl http://localhost:8080/scenarioo-$BRANCH/rest/builds/updateAndImport
