01e9b029932798de01d0ce95b41203ba51fd9b4b
danielsuter <suter_daniel@hotmail.com>
2017-10-20 15:00:58 +0200

    Cleanup of unused dependencies.

--- scenarioo-client/app/vendor.js
+++
@@ -1,6 +0,0 @@
-
-// require('./components/svg.js/dist/svg.js');
-// require('./components/svg.select.js/dist/svg.select.js');
-// require('./components/svg.draggable.js/dist/svg.draggable.js');
-// require('./components/svg.resize.js/dist/svg.resize.js');
-// require('./components/svg-pan-zoom/dist/svg-pan-zoom.js');
