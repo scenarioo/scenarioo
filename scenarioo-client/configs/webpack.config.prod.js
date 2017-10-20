35512e1dde0ffed3165b4db452ec2a6084846796
danielsuter <suter_daniel@hotmail.com>
2017-10-20 15:56:42 +0200

    Use uglify for prod build. Stop on errors.

--- scenarioo-client/configs/webpack.config.prod.js
+++ scenarioo-client/configs/webpack.config.prod.js
@@ -1,8 +1,12 @@
+var webpack = require('webpack');
 var merge = require('webpack-merge');
 var commonConfig = require('./webpack.config.common');
 
 var webpackConfig = merge(commonConfig, {
-
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.optimize.UglifyJsPlugin()
+    ]
 });
 
 
