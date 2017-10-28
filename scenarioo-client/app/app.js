66368bbea95efec8d5d3e1f740fd3ff608cddb26
danielsuter <suter_daniel@hotmail.com>
2017-10-28 17:24:51 +0200

    Extract routes to a separate file.

--- scenarioo-client/app/app.js
+++ scenarioo-client/app/app.js
@@ -44,88 +44,12 @@ angular.module('scenarioo.services', ['ngResource', 'ngRoute', 'LocalStorageModu
 angular.module('scenarioo.controllers', ['scenarioo.services', 'scenarioo.directives']);
 require('./shared/utils/number.filter');
 
+var addRoutes = require('./app.routes');
+
 angular.module('scenarioo', ['scenarioo.controllers', 'ui.bootstrap', 'scenarioo.screenAnnotations'])
 
     .config(function ($routeProvider) {
-
-        /**
-         * breadcrumbId: id of the breadcrumb elements to use for this page as defined in breadcrumbs.service.js
-         */
-        $routeProvider
-            .when('/', {
-                template: require('./build/build.html'),
-                controller: 'BuildController',
-                controllerAs: 'main',
-                breadcrumbId: 'main'
-            })
-            .when('/manage', {
-                template: require('./manage/manage.html'),
-                controller: 'ManageController',
-                controllerAs: 'vm',
-                breadcrumbId: 'manage'
-            })
-            .when('/usecase/:useCaseName', {
-                template: require('./useCase/usecase.html'),
-                controller: 'UseCaseController',
-                controllerAs: 'useCase',
-                useCaseName: '@useCaseName',
-                breadcrumbId: 'usecase'
-            })
-            .when('/scenario/:useCaseName/:scenarioName', {
-                template: require('./scenario/scenario.html'),
-                controller: 'ScenarioController',
-                controllerAs: 'vm',
-                useCaseName: '@useCaseName',
-                scenarioName: '@scenarioName',
-                breadcrumbId: 'scenario'
-            })
-            .when('/search/:searchTerm', {
-                template: require('./search/search.html'),
-                controller: 'SearchController',
-                controllerAs: 'vm',
-                breadcrumbId: 'search',
-                searchTerm: '@searchTerm'
-            })
-            .when('/object/:objectType/:objectName', {
-                template: require('./objectRepository/objectRepository.html'),
-                controller: 'ObjectRepositoryController',
-                controllerAs: 'vm',
-                objectType: '@objectType',
-                objectName: '@objectName',
-                breadcrumbId: 'object'
-            })
-            .when('/step/:useCaseName/:scenarioName/:pageName/:pageOccurrence/:stepInPageOccurrence', {
-                template: require('./step/step.html'),
-                controller: 'StepController',
-                useCaseName: '@useCaseName',
-                scenarioName: '@scenarioName',
-                pageName: '@pageName',
-                pageOccurrence: '@pageOccurrence',
-                stepInPageOccurrence: '@stepInPageOccurrence',
-                breadcrumbId: 'step'
-            })
-            .when('/stepsketch/:issueId/:scenarioSketchId/:stepSketchId', {
-                template: require('./sketcher/stepSketch.html'),
-                controller: 'StepSketchController',
-                controllerAs: 'vm',
-                breadcrumbId: 'stepsketch'
-            })
-            .when('/editor', {
-                template: require('./sketcher/sketcherEditor.html'),
-                controller: 'SketcherEditorController',
-                controllerAs: 'vm'
-            })
-            .when('/editor/:issueId/:scenarioSketchId/:stepSketchId', {
-                template: require('./sketcher/sketcherEditor.html'),
-                controller: 'SketcherEditorController',
-                controllerAs: 'vm',
-                issueId: '@issueId',
-                scenarioSketchId: '@scenarioSketchId',
-                stepSketchId: '@stepSketchId'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
+        addRoutes($routeProvider);
 
     }).run(function ($rootScope, ConfigService, GlobalHotkeysService, $location, $uibModalStack, ApplicationInfoPopupService, $templateCache) {
 
