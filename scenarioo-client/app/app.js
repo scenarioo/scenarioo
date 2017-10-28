f4f5624bec6e666257901164adf5516179336f8e
danielsuter <suter_daniel@hotmail.com>
2017-10-28 17:29:10 +0200

    Move vendor imports to a separate file.

--- scenarioo-client/app/app.js
+++ scenarioo-client/app/app.js
@@ -16,26 +16,10 @@
  */
 
 
-require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
-require('./components/font-awesome/css/font-awesome.min.css');
+require('./vendor.js');
+
 require('./styles/scenarioo.less');
 
-/** vendor */
-require('jquery');
-require('angular');
-require('angular-resource');
-require('angular-sanitize');
-require('angular-route');
-require('angular-ui-bootstrap');
-require('angular-local-storage');
-require('twigs');
-
-require('./components/angular-unsavedChanges/dist/unsavedChanges.js');
-require('./components/svg.select.js/dist/svg.select.css');
-window.SVG = require('./components/svg.js/dist/svg');
-require('./components/svg.select.js/dist/svg.select');
-require('./components/svg.draggable.js/dist/svg.draggable');
-require('./components/svg.resize.js/dist/svg.resize');
 
 angular.module('scenarioo.filters', []);
 angular.module('scenarioo.screenAnnotations', ['scenarioo.filters', 'ngRoute']);
