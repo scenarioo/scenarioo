3155d254b137bb4451b904e263d42a731f9f99f3
Michael Werner <michael.werner@windowslive.com>
2018-03-21 11:00:14 +0100

    #650 Fixed comparison result label not being mapped correctly
    Signed-off-by: Michael Werner <michael.werner@windowslive.com>

--- scenarioo-server/src/main/java/org/scenarioo/rest/comparisons/ComparisonsResource.java
+++ scenarioo-server/src/main/java/org/scenarioo/rest/comparisons/ComparisonsResource.java
@@ -8,6 +8,7 @@ import org.scenarioo.rest.base.BuildIdentifier;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -18,17 +19,26 @@ public class ComparisonsResource {
 	@GET
 	@Produces({"application/xml", "application/json"})
 	public List<ComparisonResult> listAllComparisonResults() {
+		return Arrays.asList(
+			createComparisonResult("feature-650-diff-list", "2017-11-29", "To last develop",
+				ComparisonCalculationStatus.SUCCESS, 22.3542),
+			createComparisonResult("feature-650-diff-list", "2017-11-30", "To last develop",
+				ComparisonCalculationStatus.PROCESSING, 10.5));
+	}
+
+	private ComparisonResult createComparisonResult(String branchName, String buildName, String comparisonName,
+													ComparisonCalculationStatus calculationStatus, double changeRate) {
 		ComparisonResult comparisonResult = new ComparisonResult();
-		comparisonResult.setBaseBuild(new BuildIdentifier("feature-650-diff-list", "2017-11-29"));
+		comparisonResult.setBaseBuild(new BuildIdentifier(branchName, buildName));
 		comparisonResult.setCompareBuild(new BuildIdentifier("develop", "2017-11-28"));
 		ComparisonConfiguration comparisonConfiguration = new ComparisonConfiguration();
-		comparisonConfiguration.setName("To last develop");
+		comparisonConfiguration.setName(comparisonName);
 		comparisonResult.setComparisonConfiguration(comparisonConfiguration);
-		comparisonResult.setChangeRate(22.3542);
+		comparisonResult.setChangeRate(changeRate);
 		comparisonResult.setBaseBuildDate(new Date());
 		comparisonResult.setCalculationDate(new Date());
-		comparisonResult.setStatus(ComparisonCalculationStatus.SUCCESS);
-		return Collections.singletonList(comparisonResult);
+		comparisonResult.setStatus(calculationStatus);
+		return comparisonResult;
 	}
 
 }
