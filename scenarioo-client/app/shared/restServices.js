3a21dfea6d7240c665544a16dd3787ff3ad93202
Sascha Kaufmann <sascha.kaufmann92@gmail.com>
2018-05-08 15:28:28 +0200

    #650 load comparison log in comparison detail view

--- scenarioo-client/app/shared/restServices.js
+++ scenarioo-client/app/shared/restServices.js
@@ -66,6 +66,23 @@ angular.module('scenarioo.services')
         return buildImportService;
     })
 
+    .factory('ComparisonLogResource', function (ScenariooResource) {
+        return ScenariooResource('/builds/:branchName/:buildName/comparisons/:comparisonName/log',
+            {
+                branchName: '@branchName',
+                buildName: '@buildName',
+                comparisonName: '@comparisonName'
+            }, {
+                get: {
+                    method: 'GET',
+                    headers: { 'Accept': 'text/plain'},
+                    transformResponse: function(data) {
+                        return {content: data};
+                    }
+                }
+            });
+    })
+
     .factory('PageVariantService', function (ScenariooResource, $q) {
         var pageVariantService = ScenariooResource('/branch/:branchName/build/:buildName/search/pagevariants/',
             {
