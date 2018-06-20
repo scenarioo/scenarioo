ec81a624a757a0736c14975d8bf5f36207f5c94f
Michael Werner <michael.werner@windowslive.com>
2018-06-20 13:43:39 +0200

    #724: Introduced Spring Boot plugins in Gradle build

---
+++ scenarioo-server/src/main/java/org/scenarioo/ScenariooViewerApplication.java
@@ -0,0 +1,12 @@
+package org.scenarioo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScenariooViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScenariooViewerApplication.class, args);
+	}
+}
