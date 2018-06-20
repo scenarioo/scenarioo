6b53766a34099d2fad60e33f15a0189d154bfcfb
Michael Werner <michael.werner@windowslive.com>
2018-06-20 16:03:22 +0200

    #724: Migrated first REST service to Spring MVC rest controller, included static resources in WAR file

--- scenarioo-server/src/main/java/org/scenarioo/ScenariooViewerApplication.java
+++ scenarioo-server/src/main/java/org/scenarioo/ScenariooViewerApplication.java
@@ -1,12 +1,19 @@
 package org.scenarioo;
 
+import org.scenarioo.rest.application.ScenariooWebApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ScenariooViewerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScenariooViewerApplication.class, args);
+	}
+
+	@Bean
+	public ScenariooWebApplication initializeApplication() {
+		return new ScenariooWebApplication();
 	}
 }
