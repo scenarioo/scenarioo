6f6b54581c6a2c074934a9fe3459702abcb314d3
Michael Werner <michael.werner@windowslive.com>
2018-06-21 14:15:57 +0200

    #724: Added log statement to RestExceptionHandler

--- scenarioo-server/src/main/java/org/scenarioo/rest/base/exceptions/RestExceptionHandler.java
+++ scenarioo-server/src/main/java/org/scenarioo/rest/base/exceptions/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.scenarioo.rest.base.exceptions;
 
+import org.apache.log4j.Logger;
 import org.scenarioo.api.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +12,11 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
+	private Logger LOGGER = Logger.getLogger(RestExceptionHandler.class);
+
 	@ExceptionHandler(value = {ResourceNotFoundException.class})
-	public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e, WebRequest request) {
-		return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+	public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest request) {
+		LOGGER.warn(exception.getMessage());
+		return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
 	}
 }
