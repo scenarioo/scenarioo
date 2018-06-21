0870117f8ca256f3ed928c5ee8dfa1c7b3819d78
David Eggerschwiler <mister_x4@hotmail.com>
2018-06-21 14:30:17 +0200

    #724 migrate Jackson Dependencies from 1.x to 2.x (package changed from org.codehaus.jackson to com.fasterxml.jackson).

--- scenarioo-server/src/main/java/org/scenarioo/rest/base/StepIdentifier.java
+++ scenarioo-server/src/main/java/org/scenarioo/rest/base/StepIdentifier.java
@@ -12,16 +12,16 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlTransient;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.codehaus.jackson.annotate.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /**
  * Contains all the properties needed to identify a step unambiguously.
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 public class StepIdentifier {
-	
+
 	private static final String URI_ENCODING = "UTF-8";
-	
+
 	@XmlTransient
 	private ScenarioIdentifier scenarioIdentifier;
 
@@ -29,7 +29,7 @@ public class StepIdentifier {
 	int pageOccurrence;
 	int stepInPageOccurrence;
 	private Set<String> labels;
-	
+
 	private enum RedirectType {
 		STEP, SCREENSHOT
 	}
@@ -46,18 +46,18 @@ public class StepIdentifier {
 		this(new ScenarioIdentifier(buildIdentifier, usecaseName, scenarioName), pageName, pageOccurrence,
 				stepInPageOccurrence);
 	}
-	
+
 	public StepIdentifier(final BuildIdentifier buildIdentifier, final String usecaseName, final String scenarioName,
 			final String pageName, final int pageOccurrence, final int stepInPageOccurrence, final Set<String> labels) {
 		this(new ScenarioIdentifier(buildIdentifier, usecaseName, scenarioName), pageName, pageOccurrence,
 				stepInPageOccurrence, labels);
 	}
-	
+
 	public StepIdentifier(final ScenarioIdentifier scenarioIdentifier, final String pageName, final int pageOccurrence,
 			final int stepInPageOccurrence) {
 		this(scenarioIdentifier, pageName, pageOccurrence, stepInPageOccurrence, null);
 	}
-	
+
 	public StepIdentifier(final ScenarioIdentifier scenarioIdentifier, final String pageName, final int pageOccurrence,
 			final int stepInPageOccurrence, final Set<String> labels) {
 		this.scenarioIdentifier = scenarioIdentifier;
@@ -66,19 +66,19 @@ public class StepIdentifier {
 		this.stepInPageOccurrence = stepInPageOccurrence;
 		this.labels = labels;
 	}
-	
+
 	public static StepIdentifier withDifferentStepInPageOccurrence(final StepIdentifier stepIdentifier,
 			final int stepInPageOccurrence) {
 		return new StepIdentifier(stepIdentifier.getScenarioIdentifier(), stepIdentifier.getPageName(),
 				stepIdentifier.getPageOccurrence(), stepInPageOccurrence);
 	}
-	
+
 	public static StepIdentifier withDifferentIds(final StepIdentifier stepIdentifier, final int pageOccurrence,
 			final int stepInPageOccurrence) {
 		return new StepIdentifier(stepIdentifier.getScenarioIdentifier(), stepIdentifier.getPageName(), pageOccurrence,
 				stepInPageOccurrence);
 	}
-	
+
 	/**
 	 * Returns the same page in a different scenario of the same use case.
 	 */
@@ -88,22 +88,22 @@ public class StepIdentifier {
 		return new StepIdentifier(originalStepIdentifier.getBuildIdentifier(), fallbackUsecaseName,
 				fallbackScenarioName, originalStepIdentifier.getPageName(), pageOccurrence, stepInPageOccurrence);
 	}
-	
+
 	public StepIdentifier withDifferentBuildIdentifier(final BuildIdentifier buildIdentifierBeforeAliasResolution) {
 		return new StepIdentifier(getScenarioIdentifier().withDifferentBuildIdentifier(
 				buildIdentifierBeforeAliasResolution), pageName, pageOccurrence, stepInPageOccurrence);
 	}
-	
+
 	@JsonIgnore
 	public BuildIdentifier getBuildIdentifier() {
 		return scenarioIdentifier.getBuildIdentifier();
 	}
-	
+
 	@JsonIgnore
 	public ScenarioIdentifier getScenarioIdentifier() {
 		return scenarioIdentifier;
 	}
-	
+
 	@JsonIgnore
 	public void setScenarioIdentifier(final ScenarioIdentifier scenarioIdentifier) {
 		this.scenarioIdentifier = scenarioIdentifier;
@@ -113,7 +113,7 @@ public class StepIdentifier {
 	public String getBranchName() {
 		return scenarioIdentifier.getBranchName();
 	}
-	
+
 	public void setBranchName(final String branchName) {
 		scenarioIdentifier.setBranchName(branchName);
 	}
@@ -122,7 +122,7 @@ public class StepIdentifier {
 	public String getBuildName() {
 		return scenarioIdentifier.getBuildName();
 	}
-	
+
 	public void setBuildName(final String buildName) {
 		scenarioIdentifier.setBuildName(buildName);
 	}
@@ -131,7 +131,7 @@ public class StepIdentifier {
 	public String getUsecaseName() {
 		return scenarioIdentifier.getUsecaseName();
 	}
-	
+
 	public void setUsecaseName(final String usecaseName) {
 		scenarioIdentifier.setUsecaseName(usecaseName);
 	}
@@ -140,7 +140,7 @@ public class StepIdentifier {
 	public String getScenarioName() {
 		return scenarioIdentifier.getScenarioName();
 	}
-	
+
 	public void setScenarioName(final String scenarioName) {
 		scenarioIdentifier.setScenarioName(scenarioName);
 	}
@@ -148,7 +148,7 @@ public class StepIdentifier {
 	public String getPageName() {
 		return pageName;
 	}
-	
+
 	public void setPageName(final String pageName) {
 		this.pageName = pageName;
 	}
@@ -156,7 +156,7 @@ public class StepIdentifier {
 	public int getPageOccurrence() {
 		return pageOccurrence;
 	}
-	
+
 	public void setPageOccurrence(final int pageOccurrence) {
 		this.pageOccurrence = pageOccurrence;
 	}
@@ -164,7 +164,7 @@ public class StepIdentifier {
 	public int getStepInPageOccurrence() {
 		return stepInPageOccurrence;
 	}
-	
+
 	public void setStepInPageOccurrence(final int stepInPageOccurrence) {
 		this.stepInPageOccurrence = stepInPageOccurrence;
 	}
@@ -172,7 +172,7 @@ public class StepIdentifier {
 	public Set<String> getLabels() {
 		return labels;
 	}
-	
+
 	public void setLabels(final Set<String> labels) {
 		this.labels = labels;
 	}
@@ -184,12 +184,12 @@ public class StepIdentifier {
 	public URI getScreenshotUriForRedirect(final String screenshotFileNameExtension) {
 		return createUriForRedirect(RedirectType.SCREENSHOT, screenshotFileNameExtension);
 	}
-	
+
 	@JsonIgnore
 	public URI getStepUriForRedirect() {
 		return createUriForRedirect(RedirectType.STEP, null);
 	}
-	
+
 	private URI createUriForRedirect(final RedirectType redirectType, final String screenshotFileNameExtension) {
 		try {
 			return getStepUri(redirectType, screenshotFileNameExtension);
@@ -199,11 +199,11 @@ public class StepIdentifier {
 			throw new RuntimeException("Encoding " + URI_ENCODING + " for Uri is not supported", e);
 		}
 	}
-	
+
 	private URI getStepUri(final RedirectType redirectType, final String screenshotFileNameExtension)
 			throws UnsupportedEncodingException, URISyntaxException {
 		StringBuilder uriBuilder = new StringBuilder();
-		
+
 		uriBuilder.append("/rest/branch/").append(encode(getBranchName()));
 		uriBuilder.append("/build/").append(encode(getBuildName()));
 		uriBuilder.append("/usecase/").append(encode(getUsecaseName()));
@@ -211,25 +211,25 @@ public class StepIdentifier {
 		uriBuilder.append("/pageName/").append(encode(getPageName()));
 		uriBuilder.append("/pageOccurrence/").append(encode(Integer.toString(getPageOccurrence())));
 		uriBuilder.append("/stepInPageOccurrence/").append(encode(Integer.toString(getStepInPageOccurrence())));
-		
+
 		if (RedirectType.SCREENSHOT.equals(redirectType)) {
 			uriBuilder.append("/image." + screenshotFileNameExtension);
 		}
-		
+
 		uriBuilder.append("?fallback=true");
-		
+
 		return new URI(uriBuilder.toString());
 	}
-	
+
 	private String encode(final String urlParameter) throws UnsupportedEncodingException {
 		String encoded = URLEncoder.encode(urlParameter, URI_ENCODING);
 		return encoded.replace("+", "%20");
 	}
-	
+
 	@Override
 	public String toString() {
 		return new ToStringBuilder(this).append(scenarioIdentifier).append(pageName).append(pageOccurrence)
 				.append(stepInPageOccurrence).append(labels).build();
 	}
-	
+
 }
