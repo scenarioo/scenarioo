a6919292c70bbb5cd5f433110c0c92c7e894eaa2
Ivo de Concini <ivo.deconcini@zuehlke.com>
2018-06-22 14:00:14 +0200

    Fix errors after merge

--- scenarioo-client/app/vendor.ts
+++ scenarioo-client/app/vendor.ts
@@ -1,5 +1,5 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './components/font-awesome/css/font-awesome.min.css';
+import './fontawesome-icons';
 
 import 'jquery';
 import 'angular';
@@ -10,9 +10,8 @@ import 'angular-ui-bootstrap';
 import 'angular-local-storage';
 import 'twigs';
 
-import './components/angular-unsavedChanges/dist/unsavedChanges.js';
-import './components/svg.select.js/dist/svg.select.css';
-(<any>window).SVG = require('./components/svg.js/dist/svg');
-import './components/svg.select.js/dist/svg.select';
-import './components/svg.draggable.js/dist/svg.draggable';
-import './components/svg.resize.js/dist/svg.resize';
+import '../node_modules/svg.select.js/dist/svg.select.css';
+(<any>window).SVG = require('../node_modules/svg.js/dist/svg');
+import '../node_modules/svg.select.js/dist/svg.select';
+import '../node_modules/svg.draggable.js/dist/svg.draggable';
+import '../node_modules/svg.resize.js/dist/svg.resize';
