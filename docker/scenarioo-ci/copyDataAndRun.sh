c260706ca4b0ac55dba6dfdd5739b74b634d6f69
Christoph Gross <gross@gross-it.com>
2018-10-17 16:33:16 +0200

    #755 Fix copying data from container to /scenarioo/data

--- docker/scenarioo-ci/copyDataAndRun.sh
+++ docker/scenarioo-ci/copyDataAndRun.sh
@@ -3,7 +3,10 @@
 # To keep data of multiple builds we copy the current build data into the
 # mounted scenarioo data directory. This way we complement already existing data
 # with the lastest test run results bundled with this image.
+echo "Copy E2E scenarioo data..."
+mkdir -p /scenarioo/data
 cp -pR /scenarioo/currentBuildData/* /scenarioo/data
 
 # Start tomcat
+echo "DONE - Start tomcat"
 $CATALINA_HOME/bin/catalina.sh run
