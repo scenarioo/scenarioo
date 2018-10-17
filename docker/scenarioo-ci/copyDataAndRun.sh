96018803e8e6898ee7e534cd44e56f2d3ac7079a
Christoph Gross <gross@gross-it.com>
2018-10-17 11:32:55 +0200

    #755 Enable published docker image to show data of multiple runs by merging current test results with data mounted in /scenarioo/data

---
+++ docker/scenarioo-ci/copyDataAndRun.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# To keep data of multiple builds we copy the current build data into the
+# mounted scenarioo data directory. This way we complement already existing data
+# with the lastest test run results bundled with this image.
+cp -pR /scenarioo/currentBuildData /scenarioo/data
+
+# Start tomcat
+$CATALINA_HOME/catalina.sh run
