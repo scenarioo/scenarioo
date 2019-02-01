173819ae417a6ef4a2bfaeceb1d760886621cb99
David Eggerschwiler <mister_x4@hotmail.com>
2019-02-01 09:37:54 +0100

    #787 remove unused import

--- scenarioo-server/src/main/java/org/scenarioo/rest/builds/BuildsResource.java
+++ scenarioo-server/src/main/java/org/scenarioo/rest/builds/BuildsResource.java
@@ -34,7 +34,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.annotation.MultipartConfig;
 import java.io.File;
 import java.util.List;
 
