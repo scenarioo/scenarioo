a42e0b7f3cea595e6a0998b97d0d3a345debe872
Sebastian Zillessen <sebastian@zillessen.info>
2019-03-03 18:13:20 +0100

    Remove unused imports and cleaning up.

--- scenarioo-client/app/shared/services/restControllerModule.ts
+++ scenarioo-client/app/shared/services/restControllerModule.ts
@@ -1,11 +1,7 @@
 import {NgModule} from "@angular/core";
-import {LabelMetadataComponent} from "../../step/label-metadata/label-metadata.component";
-import {BrowserModule} from "@angular/platform-browser";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {FormsModule} from "@angular/forms";
 import {UpgradeModule} from "@angular/upgrade/static";
 import {EmptyResponseBodyErrorInterceptor} from "./emptyResponseBodyErrorInterceptor";
-import {LabelConfigurationService} from "../../services/label-configuration.service";
 import {VersionResource} from "./versionResource.service";
 import {SearchEngineStatusService} from "./searchEngineStatus.service";
 import {ApplicationStatusService} from "./applicationStatus.service";
@@ -13,11 +9,8 @@ import {BranchAliasesResource} from "./branchAliasResource.service";
 import {BranchesResource} from "./branchesResource.service";
 
 @NgModule({
-    declarations: [
-
-    ],
-    entryComponents: [
-    ],
+    declarations: [],
+    entryComponents: [],
     imports: [
         HttpClientModule,
         UpgradeModule,
