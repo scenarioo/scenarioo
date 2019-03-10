f2f871a32ecd45a1187e7d53d0ea88f6563b1ce2
Sebastian Zillessen <sebastian@zillessen.info>
2019-03-10 13:39:58 +0100

    Revert: Migrate CustomTabContentResource to Angular Service. (fb07282) - Modification by accident

--- scenarioo-server/src/main/java/org/scenarioo/rest/objectRepository/CustomTabsResource.java
+++ scenarioo-server/src/main/java/org/scenarioo/rest/objectRepository/CustomTabsResource.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/rest/branches/{branchName}/builds/{buildName}/v/{tabId}")
+@RequestMapping("/rest/branches/{branchName}/builds/{buildName}/customTabObjects/{tabId}")
 public class CustomTabsResource extends AbstractBuildContentResource {
 
 	@GetMapping
