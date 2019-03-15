4a8b836b3b865b70744779365e7aac3c9633efd4
Kevin Gerspacher <kevin.gerspacher@zuehlke.com>
2019-03-15 15:03:18 +0100

    Added component for labelColors and converted AngularJS to Angular Fixed issue with delete empty rows Fixed issue with 'missing color selection' hint

--- scenarioo-client/app/manage/index.ts
+++ scenarioo-client/app/manage/index.ts
@@ -1,11 +1,18 @@
+import {downgradeComponent} from '@angular/upgrade/static';
+import {LabelColorsComponent} from './labelColors/labelColors.component';
+
+declare var angular: angular.IAngularStatic;
+
 import './config.service';
 import './manage.controller';
 import './buildImport/buildsList.controller';
 import './buildImport/buildImportDetails.controller';
 import './generalSettings/generalSettings.controller';
 import './branchAliases/branchAliases.controller';
-import './labelColors/labelColors.controller';
 import './comparisons/comparisons.controller';
 import './comparisons/comparisonDetails.controller';
 import './comparisons/createComparisonModal.controller';
 import './comparisons/comparisonStatusMapper.service';
+
+angular.module('scenarioo.directives')
+    .directive('labelColors', downgradeComponent({component: LabelColorsComponent}) as angular.IDirectiveFactory);
---
+++ scenarioo-client/app/manage/labelColors/labelColors.component.ts
@@ -0,0 +1,103 @@
+import {Component, OnInit} from '@angular/core';
+import {LabelConfigurationsListResource} from '../../shared/services/labelConfigurationsListResource.service';
+import {LabelConfigurationsResource} from '../../shared/services/labelConfigurationsResource.service';
+
+@Component({
+    selector: 'label-colors',
+    template: require('./labelColors.component.html'),
+    styles: [require('./labelColors.component.css').toString()],
+})
+
+export class LabelColorsComponent implements OnInit {
+    availableColors = [{backgroundColor: '#e11d21', foregroundColor: '#FFFFFF'},
+        {backgroundColor: '#eb6420', foregroundColor: '#FFFFFF'},
+        {backgroundColor: '#fbca04', foregroundColor: '#000000'},
+        {backgroundColor: '#009800', foregroundColor: '#FFFFFF'},
+        {backgroundColor: '#006b75', foregroundColor: '#FFFFFF'},
+        {backgroundColor: '#207de5', foregroundColor: '#FFFFFF'},
+        {backgroundColor: '#0052cc', foregroundColor: '#FFFFFF'},
+        {backgroundColor: '#5319e7', foregroundColor: '#FFFFFF'}];
+
+    labelConfigurations = [];
+    colorMissing = [];
+    successfullyUpdatedLabelConfigurations = false;
+    labelConfigurationsListResource: LabelConfigurationsListResource;
+    labelConfigurationsResource: LabelConfigurationsResource;
+
+    constructor(labelConfigurationsListResource: LabelConfigurationsListResource, labelConfigurationsResource: LabelConfigurationsResource) {
+        this.labelConfigurationsListResource = labelConfigurationsListResource;
+        this.labelConfigurationsResource = labelConfigurationsResource;
+    }
+
+    ngOnInit(): void {
+        this.loadLabelConfigurations();
+    }
+
+    loadLabelConfigurations() {
+        this.labelConfigurationsListResource.query()
+            .subscribe((labelConfigurations) => {
+                labelConfigurations.push(LabelColorsComponent.createEmptyLabelConfiguration());
+                this.labelConfigurations = labelConfigurations;
+            });
+    }
+
+    static createEmptyLabelConfiguration() {
+        return {name: '', backgroundColor: '', foregroundColor: ''};
+    }
+
+    deleteEntry(selectedLabelConfiguration) {
+        if (selectedLabelConfiguration.name !== '' || selectedLabelConfiguration !== this.labelConfigurations[this.labelConfigurations.length - 1]) {
+
+            for (let index = 0; index < this.labelConfigurations.length; index++) {
+                const currentConfiguration = this.labelConfigurations[index];
+                if (currentConfiguration.name === selectedLabelConfiguration.name) {
+                    this.labelConfigurations.splice(index, 1);
+                    break;
+                }
+            }
+        }
+    }
+
+    addNewRow() {
+        const labelName = this.labelConfigurations[this.labelConfigurations.length - 1].name;
+        if (labelName !== '') {
+            this.labelConfigurations.push(LabelColorsComponent.createEmptyLabelConfiguration());
+        }
+    }
+
+    reset() {
+        this.colorMissing = [];
+        this.loadLabelConfigurations();
+    }
+
+    save() {
+        this.colorMissing = [];
+        const labelConfigurationsAsMap = {};
+        let everythingIsValid = true;
+        this.labelConfigurations.forEach((value, key) => {
+            if (value.name !== '') {
+                if (!value.backgroundColor) {
+                    everythingIsValid = false;
+                    this.colorMissing[key] = true;
+                }
+                labelConfigurationsAsMap[value.name] = {
+                    backgroundColor: value.backgroundColor,
+                    foregroundColor: value.foregroundColor,
+                };
+            }
+        });
+
+        if (everythingIsValid) {
+            this.labelConfigurationsResource.save(labelConfigurationsAsMap)
+                .subscribe(() => {
+                    this.successfullyUpdatedLabelConfigurations = true;
+                });
+        }
+    }
+
+    selectColor(labelConfiguration, color) {
+        labelConfiguration.backgroundColor = color.backgroundColor;
+        labelConfiguration.foregroundColor = color.foregroundColor;
+    }
+
+}
