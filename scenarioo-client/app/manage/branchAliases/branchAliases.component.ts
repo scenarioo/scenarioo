a312d6ce3c6b0643a013e7963352c1d30fc2c1e5
Kevin Gerspacher <kevin.gerspacher@zuehlke.com>
2019-03-18 12:54:40 +0100

    Updated HTML file Added first methods to ts

--- scenarioo-client/app/manage/branchAliases/branchAliases.component.ts
+++ scenarioo-client/app/manage/branchAliases/branchAliases.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {BranchesResource} from "../../shared/services/branchesResource.service";
 
 @Component({
-    selector: 'label-colors',
+    selector: 'branch-aliases',
     template: require('./branchAliases.component.html'),
     styles: [require('./branchAliases.component.css').toString()],
 })
@@ -13,6 +14,27 @@ export class BranchAliasesComponent implements OnInit {
     uniqueError = false;
     successfullyUpdatedBranchAliases = false;
 
+    branchResource: BranchesResource;
+
+    constructor(branchResource: BranchesResource){
+        this.branchResource = branchResource;
+    }
+
+
+
     ngOnInit(): void {
+    }
+
+    loadBranchesWithoutAliases(): void {
+        this.branchResource.query().subscribe((branches) => {
+            const branchesWithoutAliases = [];
+            for(let index = 0; index < branches.length; index++){
+                const branch = branches[index];
+                if(!branch.isAlias){
+                    branchesWithoutAliases.push(branch);
+                }
+            }
+            this.branches = branchesWithoutAliases;
+        });
     }
 }
