e146aea7af79fae97895557289662ad0470ceed8
Kevin Gerspacher <kevin.gerspacher@zuehlke.com>
2019-03-19 13:33:45 +0100

    Added branchAliasService

---
+++ scenarioo-client/app/shared/services/branchAlias.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from "@angular/core";
+import {Subject} from "rxjs";
+import {downgradeInjectable} from "@angular/upgrade/static";
+
+declare var angular: angular.IAngularStatic;
+
+@Injectable()
+export class BranchAliasService {
+
+    branchesLoadedSubject = new Subject<boolean>();
+    branchesLoaded$ = this.branchesLoadedSubject.asObservable();
+
+}
+
+angular.module('scenarioo.services')
+    .factory('BranchAliasService', downgradeInjectable(BranchAliasService));
