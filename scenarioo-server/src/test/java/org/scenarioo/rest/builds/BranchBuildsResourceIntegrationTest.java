e2e477559e9cd94b9f45fb8735ce75b694c73365
Michael Werner <michael.werner@windowslive.com>
2019-05-28 13:59:59 +0200

    #742 Use exchange method in rest template to deal with generic collections in response

--- scenarioo-server/src/test/java/org/scenarioo/rest/builds/BranchBuildsResourceIntegrationTest.java
+++ scenarioo-server/src/test/java/org/scenarioo/rest/builds/BranchBuildsResourceIntegrationTest.java
@@ -1,19 +1,24 @@
 package org.scenarioo.rest.builds;
 
-import org.junit.*;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.scenarioo.business.builds.BuildLink;
+import org.scenarioo.model.docu.aggregates.branches.BranchBuilds;
 import org.scenarioo.rest.integrationtest.AbstractIntegrationTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.*;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpMethod.GET;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -26,22 +31,27 @@ public class BranchBuildsResourceIntegrationTest extends AbstractIntegrationTest
 	@Test
 	public void listBranchesAndBuilds_should_return_defined_branches_and_builds() {
 		//act
-		ResponseEntity<List> response =
-			testRestTemplate
-				.withBasicAuth("scenarioo", "only4test")
-				.getForEntity("/rest/branches", List.class);
+		ResponseEntity<List<BranchBuilds>> response = testRestTemplate.exchange(
+			"/rest/branches", GET,
+			noRequestEntity(),
+			new ParameterizedTypeReference<List<BranchBuilds>>() {
+			});
 
 		//assert
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-		assertThat(response.getBody()).hasSize(1);
-		LinkedHashMap actual = (LinkedHashMap) response.getBody().get(0);
-		assertThat(actual.get("branch")).isInstanceOf(LinkedHashMap.class);
-		assertThat(((LinkedHashMap<String,String>)actual.get("branch")).get("name")).isEqualTo("testBranch");
-		assertThat(actual.get("builds")).isInstanceOf(List.class);
-		assertThat((List)actual.get("builds")).hasSize(4);
-		assertThat(((List) actual.get("builds")).get(0)).isInstanceOf(LinkedHashMap.class);
-		LinkedHashMap<String,String> testBuild = (LinkedHashMap<String, String>) ((List) actual.get("builds")).get(0);
-		assertThat(((LinkedHashMap<String, String>) ((List) actual.get("builds")).get(0)).get("linkName")).isEqualTo("testBuild");
+		List<BranchBuilds> actual = response.getBody();
+		assertThat(actual).hasSize(1);
+
+		BranchBuilds actualBranchBuilds = actual.get(0);
+		assertThat(actualBranchBuilds.getBranch().getName()).isEqualTo("testBranch");
+		assertThat(actualBranchBuilds.getBuilds())
+			.hasSize(4)
+			.extracting(BuildLink::getLinkName)
+			.contains("testBuild");
+	}
+
+	private HttpEntity<?> noRequestEntity() {
+		return null;
 	}
 
 }
