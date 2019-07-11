d42a0b5333bcc7b08ca2e544f8b718d8a476d847
SteffiSchlumpf <stefanie.schlumpf@outlook.com>
2019-07-11 12:50:24 +0200

    Try to make bindings work in custom-tab

--- scenarioo-client/app/build/custom-tab.directive.ts
+++ scenarioo-client/app/build/custom-tab.directive.ts
@@ -1,10 +1,14 @@
 import {UpgradeComponent} from '@angular/upgrade/static';
-import {Directive, ElementRef, Injector} from '@angular/core';
+import {Directive, ElementRef, Injector, Input} from '@angular/core';
 
 @Directive({
     selector: 'sc-custom-tab',
 })
 export class CustomTabDirective extends UpgradeComponent {
+
+    @Input() tabTitle;
+    @Input() tabColumns;
+
     constructor(elementRef: ElementRef, injector: Injector) {
         super('scCustomTab', elementRef, injector);
     }
