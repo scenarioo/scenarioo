65f22173dcefd06aa5547528ba489b7b8be2edc5
SteffiSchlumpf <stefanie.schlumpf@outlook.com>
2019-07-15 10:32:36 +0200

    Working on filter pipe.

--- scenarioo-client/app/pipes/filter.pipe.ts
+++ scenarioo-client/app/pipes/filter.pipe.ts
@@ -6,16 +6,60 @@ import { Injectable, Pipe, PipeTransform } from '@angular/core';
 
 @Injectable()
 export class FilterPipe implements PipeTransform {
-    transform(items: any[], field: string, value: string): any[] {
+    transform(items: any[], value: string): any[] {
         if (!items) {
             return [];
         }
-        if (!field || !value) {
+        if (!value) {
             return items;
         }
 
-        return items.filter((singleItem) =>
-            singleItem[field].toLowerCase().includes(value.toLowerCase()),
-        );
+        const filteredModel = [];
+
+        items.forEach((singleItem) => {
+            if(typeof singleItem === 'object') {
+                if (this.objectContainsAllSearchElements(singleItem, value)) {
+                    filteredModel.push(singleItem);
+                }
+            }
+        });
+
+        return filteredModel;
+
+    }
+
+    objectContainsAllSearchElements(object, value: string): boolean {
+        const searchElements = value.split(' ');
+
+        for (const i in searchElements) {
+            if (typeof searchElements[i] === 'string') {
+                if (!this.objectContainsString(object, searchElements[i])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    objectContainsString(object, string: string): boolean {
+        let returnTrue = false;
+
+        Object.keys(object).forEach((property) => {
+            if (!returnTrue) {
+                if (typeof property === 'string') {
+                    if (this.contains(property, string)) {
+                        returnTrue = true;
+                    }
+                } else {
+                    returnTrue = this.objectContainsString(property, string);
+                }
+            }
+        });
+
+        return returnTrue;
+    }
+
+    contains(haystack: string, needle: string): boolean {
+        return haystack.toLowerCase().indexOf(needle.toLowerCase()) > -1;
     }
 }
