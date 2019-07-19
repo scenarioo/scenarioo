586157e6f93b18cbeadde54bd46a8f9ea41045d8
SteffiSchlumpf <stefanie.schlumpf@outlook.com>
2019-07-19 08:56:47 +0200

    Create Progressbar Component.

---
+++ scenarioo-client/app/components/progressbar/progressbar.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+    selector: 'sc-progressbar',
+    template: require('./progressbar.component.html'),
+    styles: [require('./progressbar.component.css').toString()],
+})
+
+export class ProgressbarComponent {
+
+
+}
