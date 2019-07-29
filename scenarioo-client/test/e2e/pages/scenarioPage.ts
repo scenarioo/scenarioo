1b7452197cbffcb215a74c89b762d3134f6b56b1
SteffiSchlumpf <stefanie.schlumpf@outlook.com>
2019-07-29 19:42:56 +0200

    Fix Circle Ci Errors (E2E tests).

--- scenarioo-client/test/e2e/pages/scenarioPage.ts
+++ scenarioo-client/test/e2e/pages/scenarioPage.ts
@@ -22,11 +22,11 @@ class ScenarioPage {
     }
 
     async assertFirstChangedPageDiffIconHasValue() {
-        return expect($('.steps-view div:first-child .sc-step-in-overview .sc-scenario-page-title .diff-info-wrapper span').getText()).toContain('%');
+        return expect($('.step-view div:first-child .sc-step-in-overview .sc-scenario-page-title .diff-info-wrapper span').getText()).toContain('%');
     }
 
     async assertFirstChangedStepDiffIconHasValue() {
-        return expect($('.steps-view div:first-child .sc-step-in-overview:first-child .step-title span').getText()).toContain('%');
+        return expect($('.step-view div:first-child .sc-step-in-overview:first-child .step-title span').getText()).toContain('%');
     }
 
     async assertAddedPageDiffIconTextEqualsAdded() {
