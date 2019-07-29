fe1b839054f6b65f0f6dde855a5554da560044ea
SteffiSchlumpf <stefanie.schlumpf@outlook.com>
2019-07-29 21:34:15 +0200

    Fix Circle Ci Errors (Clean & install).

--- scenarioo-client/test/e2e/specs/list_steps.ts
+++ scenarioo-client/test/e2e/specs/list_steps.ts
@@ -19,7 +19,7 @@ useCase('List steps of scenario')
             .it(async () => {
                 await Utils.navigateToRoute('/scenario/Donate/find_donate_page?branch=wikipedia-docu-example&build=2014-03-19');
                 await NavigationPage.chooseComparison('To Projectstart');
-                await ScenarioPage.expandAllPages();
+                // await ScenarioPage.expandAllPages();
                 await ScenarioPage.assertFirstChangedPageDiffIconHasValue();
                 await ScenarioPage.assertFirstChangedStepDiffIconHasValue();
                 await ScenarioPage.assertAddedStepDiffIconTextEqualsAdded();
