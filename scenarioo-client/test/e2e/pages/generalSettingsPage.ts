a7b460599d4b26d6689db6dc4c5a6d7d1b2e181e
danielsuter <suter_daniel@hotmail.com>
2019-11-15 14:48:05 +0100

    Remove element check as it's not in the DOM.

--- scenarioo-client/test/e2e/pages/generalSettingsPage.ts
+++ scenarioo-client/test/e2e/pages/generalSettingsPage.ts
@@ -5,7 +5,6 @@ import * as Utils from '../util';
 
 class GeneralSettingsPage {
 
-    private searchNotConfiguredMessage = element.all(by.id('sc-search-not-configured-message'));
     private configuredSearchEndpoint = element(by.id('sc-configured-search-endpoint'));
     private configuredSearchClusterName = element(by.id('sc-configured-search-cluster-name'));
     private searchEndpointIsReachable = element(by.id('sc-search-endpoint-is-reachable'));
@@ -16,7 +15,6 @@ class GeneralSettingsPage {
     }
 
     async assertSearchEndpointConfiguredAndReachable() {
-        await expect(this.searchNotConfiguredMessage.count()).toBe(0);
         await expect(this.configuredSearchEndpoint.getText()).toBe('localhost:9300');
         await expect(this.configuredSearchClusterName.getText()).toBe('elasticsearch');
         await expect(this.searchEndpointIsNotReachable.isDisplayed()).toBeFalsy();
