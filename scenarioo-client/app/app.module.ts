e2c99c991789c9f6f3bd69529ce8548abc7ad648
danielsuter <suter_daniel@hotmail.com>
2019-11-15 14:07:48 +0100

    Remove legacy component.

--- scenarioo-client/app/app.module.ts
+++ scenarioo-client/app/app.module.ts
@@ -15,7 +15,6 @@ import {TabsModule} from 'ngx-bootstrap/tabs';
 import {ModalModule} from 'ngx-bootstrap/modal';
 import {UseCasesOverviewComponent} from './build/use-cases-overview/use-cases-overview.component';
 import {ManageTabsComponent} from './manage/manage-tabs/manage-tabs.component';
-import {GeneralSettingsDirective} from './manage/generalSettings/general-settings.directive';
 import {LabelColorsDirective} from './manage/labelColors/label-colors.directive';
 import {BuildsListDirective} from './manage/buildImport/builds-list.directive';
 import {ComparisonsDirective} from './manage/comparisons/comparisons.directive';
