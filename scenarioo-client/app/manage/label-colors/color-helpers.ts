635f0f103e724e1f3277b1b8205961329b3ed0d8
Lyndsey Bonelli <lynparbone@gmail.com>
2020-02-13 17:53:21 +0100

    #459: More label colors
    Remove contrast package, write own implementation

--- scenarioo-client/app/manage/label-colors/color-helpers.ts
+++ scenarioo-client/app/manage/label-colors/color-helpers.ts
@@ -1,5 +1,3 @@
-import * as contrast from 'contrast/index';
-
 export const isHexadecimalColorValid = (color: string): boolean => {
     // Color hexadecimal should have the format #000 or #0000000
     const hexadecimalColorRegexPattern = /^#([0-9A-F]{3}){1,2}$/i;
@@ -13,4 +11,36 @@ export const getContrastingColor = (hexColor: string): string => {
         return darkForegroundColor;
     }
     return lightForegroundColor;
+};
+
+const contrast = (hexColor: string) => {
+    const [r, g, b] = [...hexToRgb(hexColor)];
+    console.log(r + ' ' + g + ' ' + b);
+    const hsp = Math.sqrt(0.299 * r * r + 0.587 * g * g + 0.114 * b * b);
+    if (hsp > 127.5) {
+        return 'light';
+    }
+    return 'dark';
+};
+
+const hexToRgb = (hexColor: string): number[] => {
+    if (hexColor.startsWith('#')) {
+        hexColor = hexColor.replace('#', '');
+    }
+
+    if (hexColor.length === 3) {
+        hexColor = hexColor
+            .split('')
+            .reduce((accum, value) => {
+                return accum.concat([value, value])
+            }, [])
+            .join('');
+    }
+
+    console.log(hexColor);
+
+    const r: number = parseInt(hexColor.slice(0, 2), 16);
+    const g: number = parseInt(hexColor.slice(2, 4), 16);
+    const b: number = parseInt(hexColor.slice(4, 6), 16);
+    return [r, g, b];
 };
